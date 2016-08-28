8cf2096f4df5f4abafdb214d97f4cf1c57227ee8
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-08-28 11:18:55 +0200

    Rename zeroconf recipe and split out netifaces

---
+++ projects/python-zeroconf.cmake
@@ -0,0 +1,10 @@
+ExternalProject_Add(PythonZeroconf
+    DEPENDS PythonNetifaces
+    URL https://github.com/jstasiak/python-zeroconf/archive/0.17.5.tar.gz
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+    BUILD_IN_SOURCE 1
+)
+
+SetProjectDependencies(TARGET PythonZeroconf DEPENDS PythonNetifaces)
