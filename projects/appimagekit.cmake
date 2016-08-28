1ed7b57f6e2dd1831d6bab97c8c9bbd21bd97fba
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-08-28 18:27:43 +0200

    Disabling AppImageKit recipe again
    Due to a problem (reported upstream) it isn't possible to run the built binaries.
    So disabled this recipe temporary..

--- projects/appimagekit.cmake
+++ projects/appimagekit.cmake
@@ -1,24 +1,26 @@
-# Additional dependencies: libfuse-dev
-
-ExternalProject_Add(AppImageKit
-    GIT_REPOSITORY https://github.com/probonopd/AppImageKit.git
-    GIT_TAG origin/master
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-    INSTALL_COMMAND ""
-    BUILD_IN_SOURCE 1
-)
-
-# Copying all AppImageKit tools into our prefix manually
-# AppImageKit doesn't provide its own install routine...
-# (https://github.com/probonopd/AppImageKit/issues/222)
-add_custom_command(
-    TARGET AppImageKit POST_BUILD
-    COMMENT "Installing AppImageKit tools to ${CMAKE_INSTALL_PREFIX}/bin/"
-    COMMAND ${CMAKE_COMMAND} -E copy AppImageAssistant ${CMAKE_INSTALL_PREFIX}/bin/
-    COMMAND ${CMAKE_COMMAND} -E copy AppImageExtract   ${CMAKE_INSTALL_PREFIX}/bin/
-    COMMAND ${CMAKE_COMMAND} -E copy AppImageMonitor   ${CMAKE_INSTALL_PREFIX}/bin/
-    COMMAND ${CMAKE_COMMAND} -E copy AppRun            ${CMAKE_INSTALL_PREFIX}/bin/
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/AppImageKit-prefix/src/AppImageKit/
-)
-
-SetProjectDependencies(TARGET AppImageKit)
+## Issue: built AppImageKit executables are broken: https://github.com/probonopd/AppImageKit/issues/223
+#
+#ExternalProject_Add(AppImageKit
+#    GIT_REPOSITORY https://github.com/probonopd/AppImageKit.git
+#    GIT_TAG origin/master
+#    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+#    INSTALL_COMMAND ""
+#    BUILD_IN_SOURCE 1
+#)
+#
+## Copying all AppImageKit tools into our prefix manually
+## AppImageKit doesn't provide its own install routine...
+## (https://github.com/probonopd/AppImageKit/issues/222)
+#add_custom_command(
+#    TARGET AppImageKit
+#    POST_BUILD
+#    DEPENDS AppImageKit
+#    COMMENT "Installing AppImageKit tools to ${CMAKE_INSTALL_PREFIX}/bin/"
+#    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/bin/
+#    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/AppImageKit-prefix/src/AppImageKit/AppImageAssistant ${CMAKE_INSTALL_PREFIX}/bin/
+#    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/AppImageKit-prefix/src/AppImageKit/AppImageExtract   ${CMAKE_INSTALL_PREFIX}/bin/
+#    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/AppImageKit-prefix/src/AppImageKit/AppImageMonitor   ${CMAKE_INSTALL_PREFIX}/bin/
+#    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/AppImageKit-prefix/src/AppImageKit/AppRun            ${CMAKE_INSTALL_PREFIX}/bin/
+#)
+#
+#SetProjectDependencies(TARGET AppImageKit)
