3da069428609af71b9f5219444049c458ad0a51e
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-08-28 15:46:44 +0200

    Don't install pyserial as an egg

--- projects/pyserial.cmake
+++ projects/pyserial.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(PySerial
     URL_MD5 2f72100de3e410b36d575e12e82e9d27
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --old-and-unmanageable
     BUILD_IN_SOURCE 1
 )
 
