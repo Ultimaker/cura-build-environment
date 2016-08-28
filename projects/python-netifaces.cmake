8efebd91589324313113ef93139e1545a504711d
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-08-28 15:47:54 +0200

    Don't install zeroconf and netifaces as eggs

--- projects/python-netifaces.cmake
+++ projects/python-netifaces.cmake
@@ -2,7 +2,7 @@ ExternalProject_Add(PythonNetifaces
     URL https://pypi.python.org/packages/18/fa/dd13d4910aea339c0bb87d2b3838d8fd923c11869b1f6e741dbd0ff3bc00/netifaces-0.10.4.tar.gz
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --old-and-unmanageable
     BUILD_IN_SOURCE 1
 )
 
--- projects/python-zeroconf.cmake
+++ projects/python-zeroconf.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(PythonZeroconf
     URL https://github.com/jstasiak/python-zeroconf/archive/0.17.5.tar.gz
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --old-and-unmanageable
     BUILD_IN_SOURCE 1
 )
 
