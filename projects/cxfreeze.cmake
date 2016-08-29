8b7a1e4708ba82fb4801e30a5eba56ea2094e3ec
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-08-29 21:37:34 +0200

    cx_Freeze: Don't install as an egg

--- projects/cxfreeze.cmake
+++ projects/cxfreeze.cmake
@@ -2,8 +2,8 @@ ExternalProject_Add(cx_Freeze
     HG_REPOSITORY https://bitbucket.org/anthony_tuininga/cx_freeze
     CONFIGURE_COMMAND ""
     PATCH_COMMAND hg import --no-commit ${CMAKE_CURRENT_LIST_DIR}/cxfreeze_exclude_self.patch
-    BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --old-and-unmanageable
+    BUILD_COMMAND ${PYTHON_EXECUTABLE_PREFIXED} setup.py build
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE_PREFIXED} setup.py install --old-and-unmanageable
     BUILD_IN_SOURCE 1
 )
 
