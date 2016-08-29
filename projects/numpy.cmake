9e2640b14e64baead00beaf79dd2fb5bcdb9c0ef
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-08-29 21:38:18 +0200

    Numpy: Also don't install as an egg

--- projects/numpy.cmake
+++ projects/numpy.cmake
@@ -2,8 +2,8 @@ ExternalProject_Add(NumPy
     URL http://downloads.sourceforge.net/project/numpy/NumPy/1.11.1/numpy-1.11.1.tar.gz
     URL_MD5 2f44a895a8104ffac140c3a70edbd450
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --prefix=${CMAKE_INSTALL_PREFIX} --old-and-unmanageable
+    BUILD_COMMAND ${PYTHON_EXECUTABLE_PREFIXED} setup.py build
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE_PREFIXED} setup.py install --prefix=${CMAKE_INSTALL_PREFIX} --old-and-unmanageable
     BUILD_IN_SOURCE 1
 )
 
