4441f822b4230ae9df62f935a4822391ea8beb5d
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-09-01 13:59:08 +0200

    Moving env_* scripts to cura-build-environment

---
+++ env_osx.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export MACOSX_DEPLOYMENT_TARGET=10.7
+export CC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
+export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
+export CFLAGS=-I/usr/local/include
---
+++ env_win64.bat
@@ -0,0 +1,5 @@
+set PYTHONPATH=%cd%\inst\lib\python3.4\site-packages
+set PATH=%cd%\inst\bin;%PATH%
+set CFLAGS=-m64
+set CXXFLAGS=-m64
+set LDFLAGS=-m64
