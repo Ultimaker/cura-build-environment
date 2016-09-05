3a555eb1f189f9a696f1caa63784921cbe3ba130
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-09-05 17:51:46 +0200

    Removing test targets
    These are already available at cura-build itself.

--- projects/curaengine.cmake
+++
@@ -1,8 +0,0 @@
-ExternalProject_Add(CuraEngine
-    GIT_REPOSITORY https://github.com/ultimaker/CuraEngine
-    GIT_TAG origin/master
-    CMAKE_COMMAND PATH=${CMAKE_INSTALL_PREFIX}/bin/:$ENV{PATH} LD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib/ PYTHONPATH=${CMAKE_INSTALL_PREFIX}/lib/python3/dist-packages/:${CMAKE_INSTALL_PREFIX}/lib/python3.5:${CMAKE_INSTALL_PREFIX}/lib/python3.5/site-packages/ cmake
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCURA_ENGINE_VERSION=${CURA_VERSION} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-)
-
-SetProjectDependencies(TARGET CuraEngine DEPENDS Arcus)
--- projects/uranium.cmake
+++
@@ -1,7 +0,0 @@
-ExternalProject_Add(Uranium
-    GIT_REPOSITORY https://github.com/ultimaker/Uranium
-    GIT_TAG origin/master
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-)
-
-SetProjectDependencies(TARGET Uranium DEPENDS PyQt Arcus)
