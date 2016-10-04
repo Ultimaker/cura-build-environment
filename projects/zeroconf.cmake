3e5e6e218179608aa9d46630b420ca835a052817
Ghostkeeper <rubend@tutanota.com>
2016-10-04 15:30:05 +0200

    Use pyzeroconf version 0.17.6
    This version has some bugfixes for a deadlock when you reinitialise Zeroconf instances.
    
    Contributes to issue CURA-2497.

--- projects/zeroconf.cmake
+++ projects/zeroconf.cmake
@@ -19,7 +19,8 @@ ExternalProject_Add(PythonSix
 SetProjectDependencies(TARGET PythonSix DEPENDS Python)
 
 ExternalProject_Add(PythonZeroconf
-    URL https://github.com/jstasiak/python-zeroconf/archive/0.17.5.tar.gz
+    GIT_REPOSITORY https://github.com/jstasiak/python-zeroconf
+    GIT_TAG 0.17.6
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
     INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=zeroconf-install.log
