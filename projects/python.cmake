3e0fb8fdaa18447387d35065b8d4061f2a3dc78c
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-10-20 15:42:12 +0200

    Set a proper rpath for the Python executable on Linux
    Otherwise we have to add LD_LIBRARY_PATH everywhere

--- projects/python.cmake
+++ projects/python.cmake
@@ -1,12 +1,17 @@
 set(python_configure_command ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-shared --with-threads --without-pymalloc)
 
-if(APPLE)
+if(BUILD_OS_OSX)
     # See http://bugs.python.org/issue21381
     # The interpreter crashes when MACOSX_DEPLOYMENT_TARGET=10.7 due to the increased stack size.
     set(python_patch_command sed -i".bak" "9271,9271d" <SOURCE_DIR>/configure)
     # OS X 10.11 removed OpenSSL. Brew now refuses to link so we need to manually tell Python's build system
     # to use the right linker flags.
     set(python_configure_command CPPFLAGS=-I/usr/local/opt/openssl/include LDFLAGS=-L/usr/local/opt/openssl/lib ${python_configure_command})
+endif()
+
+if(BUILD_OS_LINUX)
+    # Set a proper RPATH so everything depending on Python does not need LD_LIBRARY_PATH
+    set(python_configure_command LDFLAG=-rpath=${CMAKE_INSTALL_PREFIX}/lib ${python_configure_command})
 endif()
 
 ExternalProject_Add(Python
