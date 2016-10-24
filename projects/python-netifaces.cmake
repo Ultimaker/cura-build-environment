ca96be5274b63960fe162a191fd30df977f7a4d9
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-25 00:28:30 +0200

    Netifaces: Adding Windows if-clause and comment
    Contributes to CURA-2150

--- projects/python-netifaces.cmake
+++ projects/python-netifaces.cmake
@@ -1,9 +1,13 @@
-ExternalProject_Add(PythonNetifaces
-    URL https://pypi.python.org/packages/18/fa/dd13d4910aea339c0bb87d2b3838d8fd923c11869b1f6e741dbd0ff3bc00/netifaces-0.10.4.tar.gz
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=netifaces-install.log
-    BUILD_IN_SOURCE 1
-)
-
-SetProjectDependencies(TARGET PythonNetifaces DEPENDS Python)
+# Using Netifaces installed via pip on Windows
+# Eg. "pip3 install netifaces"
+# Other OSs will build it manually...
+if(NOT BUILD_OS_WINDOWS)
+    ExternalProject_Add(PythonNetifaces
+        URL https://pypi.python.org/packages/18/fa/dd13d4910aea339c0bb87d2b3838d8fd923c11869b1f6e741dbd0ff3bc00/netifaces-0.10.4.tar.gz
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
+        INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=netifaces-install.log
+        BUILD_IN_SOURCE 1
+    )
+    SetProjectDependencies(TARGET PythonNetifaces DEPENDS Python)
+endif()
