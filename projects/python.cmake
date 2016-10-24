6b0e2d6d1fc9f0ec156acf965289e7496b09ac51
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-24 17:51:53 +0200

    Adding dummy target for Windows
    Contributes to CURA-2150

--- projects/python.cmake
+++ projects/python.cmake
@@ -14,12 +14,16 @@ if(BUILD_OS_LINUX)
     set(python_configure_command LDFLAGS=-Wl,-rpath=${CMAKE_INSTALL_PREFIX}/lib ${python_configure_command})
 endif()
 
-ExternalProject_Add(Python
-    URL https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
-    URL_MD5 3fe8434643a78630c61c6464fe2e7e72
-    PATCH_COMMAND ${python_patch_command}
-    CONFIGURE_COMMAND ${python_configure_command}
-    BUILD_IN_SOURCE 1
-)
+if(BUILD_OS_WINDOWS)
+    add_custom_target(Python)
+else()
+    ExternalProject_Add(Python
+        URL https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+        URL_MD5 3fe8434643a78630c61c6464fe2e7e72
+        PATCH_COMMAND ${python_patch_command}
+        CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-shared --with-threads --without-pymalloc
+        BUILD_IN_SOURCE 1
+    )
+endif()
 
 SetProjectDependencies(TARGET Python)
