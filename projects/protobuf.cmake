17d40e0d983e4178fa46d75c0aa2e67da8b85858
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-25 00:27:24 +0200

    Protobuf: Removing empty line
    Contributes to CURA-2150

--- projects/protobuf.cmake
+++ projects/protobuf.cmake
@@ -8,5 +8,4 @@ ExternalProject_Add(Protobuf
     URL https://github.com/google/protobuf/archive/v3.0.0.tar.gz
     CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_CXX_FLAGS=${protobuf_cxx_flags} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -G ${CMAKE_GENERATOR} ../Protobuf/cmake
 )
-
 SetProjectDependencies(TARGET Protobuf)
