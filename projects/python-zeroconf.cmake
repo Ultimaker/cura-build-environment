9ab599a8cb93cf98c4fe08910b57665921d3e844
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-25 00:31:03 +0200

    Zeroconf: Adding Windows if-clause and comment
    Contributes to CURA-2150

--- projects/python-zeroconf.cmake
+++ projects/python-zeroconf.cmake
@@ -1,9 +1,13 @@
-ExternalProject_Add(PythonZeroconf
-    URL https://github.com/jstasiak/python-zeroconf/archive/0.17.6.tar.gz
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=zeroconf-install.log
-    BUILD_IN_SOURCE 1
-)
-
-SetProjectDependencies(TARGET PythonZeroconf DEPENDS PythonNetifaces PythonSix)
+# Using Zeroconf installed via pip on Windows
+# Eg. "pip3 install zeroconf"
+# Other OSs will build it manually...
+if(NOT BUILD_OS_WINDOWS)
+    ExternalProject_Add(Python    
+        URL https://github.com/jstasiak/python-zeroconf/archive/0.17.6.tar.gz
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
+        INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=zeroconf-install.log
+        BUILD_IN_SOURCE 1
+    )
+    SetProjectDependencies(TARGET PythonZeroconf DEPENDS PythonNetifaces PythonSix)
+endif()
