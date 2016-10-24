f05d649a572d987e9066f419a89d39dccf22a2d8
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-24 18:07:59 +0200

    Splitting up numpy-stl and python-utils
    Splitting this up, because I guess that problems like
    fcbac664098ab2fd73ade40f404bf00b8b6c0536 wouldn't appear, if we had
    everything splitted up.
    
    Contributes to CURA-2150

--- projects/numpy-stl.cmake
+++ projects/numpy-stl.cmake
@@ -1,14 +1,3 @@
-ExternalProject_Add(python-utils
-    URL https://pypi.python.org/packages/24/f6/26474b0b52335864cf43a969987b7ab27ee14331def6ec82cbb8263bc937/python-utils-2.0.0.tar.gz
-    URL_MD5 05d5fba0675e0ba3e3bf367e254d5369
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=pythonutils-install.log
-    BUILD_IN_SOURCE 1
-)
-
-SetProjectDependencies(TARGET python-utils DEPENDS Python)
-
 ExternalProject_Add(NumpySTL
     URL https://github.com/WoLpH/numpy-stl/archive/v1.9.0.tar.gz
     URL_MD5 cda2531edecff4468a4bbe78c6a2833b
