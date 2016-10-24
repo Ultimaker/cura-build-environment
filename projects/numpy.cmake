97e65272cc03f81e7c59d265f8af358afd0642d9
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-25 00:29:52 +0200

    Numpy: Adding Windows if-clause and comment
    Contributes to CURA-2150

--- projects/numpy.cmake
+++ projects/numpy.cmake
@@ -1,4 +1,3 @@
-
 if(BUILD_OS_LINUX)
     # Fortran compiler is needed for OpenBLAS, but it does no check whether it is available.
     enable_language(Fortran)
@@ -15,13 +14,17 @@ else()
     add_custom_target(OpenBLAS)
 endif()
 
-ExternalProject_Add(NumPy
-    URL http://downloads.sourceforge.net/project/numpy/NumPy/1.11.1/numpy-1.11.1.tar.gz
-    URL_MD5 2f44a895a8104ffac140c3a70edbd450
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
-    BUILD_IN_SOURCE 1
-)
-
-SetProjectDependencies(TARGET NumPy DEPENDS Python OpenBLAS)
+# Using NumPy installed via pip on Windows
+# Eg. "pip3 install numpy" or via .whl
+# Other OSs will build it manually...
+if(NOT BUILD_OS_WINDOWS)
+    ExternalProject_Add(NumPy
+        URL http://downloads.sourceforge.net/project/numpy/NumPy/1.11.1/numpy-1.11.1.tar.gz
+        URL_MD5 2f44a895a8104ffac140c3a70edbd450
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
+        INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+        BUILD_IN_SOURCE 1
+    )
+    SetProjectDependencies(TARGET NumPy DEPENDS Python OpenBLAS)
+endif()
