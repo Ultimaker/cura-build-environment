111ebc92996e27f96eb585284a986de165478c7b
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-25 11:10:39 +0200

    Zeroconf: Calling target correctly
    Can't remember I killed the target name here before.
    
    Contributes to CURA-2150

--- projects/python-zeroconf.cmake
+++ projects/python-zeroconf.cmake
@@ -2,7 +2,7 @@
 # Eg. "pip3 install zeroconf"
 # Other OSs will build it manually...
 if(NOT BUILD_OS_WINDOWS)
-    ExternalProject_Add(Python    
+    ExternalProject_Add(PythonZeroconf
         URL https://github.com/jstasiak/python-zeroconf/archive/0.17.6.tar.gz
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
