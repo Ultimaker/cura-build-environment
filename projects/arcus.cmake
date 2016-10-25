e7eb7e1864c390aba89d2810e0436e32e03627b6
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-25 15:22:27 +0200

    Arcus: also do strong prefixing
    Again needed to do the prefixing here as already done in my PR #1.
    This change manages to get Arcus built on Ubuntu Yaketty.
    
    Contributes to CURA-2150

--- projects/arcus.cmake
+++ projects/arcus.cmake
@@ -1,6 +1,13 @@
+if(BUILD_OS_LINUX)
+    set(arcus_cmake_command PATH=${CMAKE_INSTALL_PREFIX}/bin/:$ENV{PATH} LD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib/ PYTHONPATH=${CMAKE_INSTALL_PREFIX}/lib/python3/dist-packages/:${CMAKE_INSTALL_PREFIX}/lib/python3.5:${CMAKE_INSTALL_PREFIX}/lib/python3.5/site-packages/ ${CMAKE_COMMAND})
+else()
+    set(arcus_cmake_command ${CMAKE_COMMAND})
+endif()
+
 ExternalProject_Add(Arcus
-    GIT_REPOSITORY https://github.com/ultimaker/libArcus
+    GIT_REPOSITORY https://github.com/ultimaker/libArcus.git
     GIT_TAG origin/master
+    CMAKE_COMMAND ${arcus_cmake_command}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX} -G ${CMAKE_GENERATOR}
 )
 
