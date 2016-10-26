7b794ce2ecca778d524aec3350583a335a525f09
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-26 15:07:53 +0200

    Protobuf: Building it static again
    Contributes to CURA-2150

--- projects/protobuf.cmake
+++ projects/protobuf.cmake
@@ -4,15 +4,9 @@ else()
     set(protobuf_cxx_flags "-fPIC -std=c++11")
 endif()
 
-if(BUILD_OS_WINDOWS)
-    set(protobuf_BUILD_SHARED_LIBS ON)
-else()
-    set(protobuf_BUILD_SHARED_LIBS OFF)
-endif()
-
 ExternalProject_Add(Protobuf
     URL https://github.com/google/protobuf/archive/v3.0.2.tar.gz
     URL_MD5 845b39e4b7681a2ddfd8c7f528299fbb
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_CXX_FLAGS=${protobuf_cxx_flags} -Dprotobuf_BUILD_SHARED_LIBS=${protobuf_BUILD_SHARED_LIBS} -Dprotobuf_WITH_ZLIB=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -G ${CMAKE_GENERATOR} ../Protobuf/cmake
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_CXX_FLAGS=${protobuf_cxx_flags} -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_WITH_ZLIB=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -G ${CMAKE_GENERATOR} ../Protobuf/cmake
 )
 SetProjectDependencies(TARGET Protobuf)
