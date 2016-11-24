344337c105bb683a6f4996ca16d3ead518b497b8
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2016-11-23 17:59:06 -0800

    Enable building of Python on Windows
    It needs OpenSSL which we now also build.
    We use the cmake Python build system since that seems to work the best.

--- projects/python.cmake
+++ projects/python.cmake
@@ -1,6 +1,4 @@
 set(python_configure_command ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-shared --with-threads --without-pymalloc)
-#set(python_cmake_COMMAND cmake)
-#set(python_cmake_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release -DPYTHON_VERSION=3.5.2 -DINSTALL_TEST=OFF -DBUILD_SHARED=ON)
 
 if(BUILD_OS_OSX)
     # See http://bugs.python.org/issue21381
@@ -17,42 +15,18 @@ if(BUILD_OS_LINUX)
 endif()
 
 if(BUILD_OS_WINDOWS)
-    ## CMAKE_* can't be used here. The cmake command above always passes the generator here. Therefore CMake will always use MinGW on Windows for Python.
-    # Needed for Python 3.5.x on Windows:
-    #set(python_cmake_ARGS ${python_cmake_ARGS} -G "Visual Studio 14 2015")
-    # Linker errors on Windows: https://github.com/python-cmake-buildsystem/python-cmake-buildsystem/issues/170
-    #ExternalProject_Add(Python
-    #    GIT_REPOSITORY https://github.com/python-cmake-buildsystem/python-cmake-buildsystem.git
-    #    GIT_TAG origin/master
-    #    CONFIGURE_COMMAND ${python_cmake_COMMAND} ${python_cmake_ARGS} ${CMAKE_BINARY_DIR}/Python-prefix/src/Python
-    #    BUILD_COMMAND ""
-    #    INSTALL_COMMAND ${python_cmake_COMMAND} --build . --target install
-    #)
+    ExternalProject_Add(Python
+       GIT_REPOSITORY https://github.com/python-cmake-buildsystem/python-cmake-buildsystem.git
+       GIT_TAG origin/master
+       CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/python -DCMAKE_BUILD_TYPE=Release -DPYTHON_VERSION=3.5.2 -DINSTALL_TEST=OFF -DINSTALL_MANUAL=OFF -DBUILD_TESTING=OFF -DBUILD_LIBPYTHON_SHARED=ON -DIS_PY3=TRUE -DOPENSSL_ROOT_DIR=${CMAKE_INSTALL_PREFIX} -DCMAKE_SHARED_LINKER_FLAGS=/SAFESEH:NO
+    )
 
-    #ExternalProject_Add(Python
-    #    URL https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
-    #    URL_MD5 3fe8434643a78630c61c6464fe2e7e72
-    #    CONFIGURE_COMMAND ""
-    #    BUILD_COMMAND PCBuild/build.bat -c Release -r -e --no-tkinter
-    #    INSTALL_COMMAND ""
-    #    BUILD_IN_SOURCE 1
-    #)
-    #add_custom_command(TARGET Python
-    #                   POST_BUILD
-    #                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/Python-prefix/src/Python/Lib ${CMAKE_INSTALL_PREFIX}
-    #                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/Python-prefix/src/Python/PCbuild/win32/py.exe ${CMAKE_INSTALL_PREFIX}/bin
-    #                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/Python-prefix/src/Python/PCbuild/win32/pyw.exe ${CMAKE_INSTALL_PREFIX}/bin
-    #                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/Python-prefix/src/Python/PCbuild/win32/python.exe ${CMAKE_INSTALL_PREFIX}/bin
-    #                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/Python-prefix/src/Python/PCbuild/win32/pythonw.exe ${CMAKE_INSTALL_PREFIX}/bin
-    #                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/Python-prefix/src/Python/PCbuild/win32/python3.dll ${CMAKE_INSTALL_PREFIX}/bin
-    #                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/Python-prefix/src/Python/PCbuild/win32/python35.dll ${CMAKE_INSTALL_PREFIX}/bin
-    #                   COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/Python-prefix/src/Python/PCbuild/win32/sqlite3.dll ${CMAKE_INSTALL_PREFIX}/DLLs
-    #                   # [...]
-    #)
+    ExternalProject_Add_Step(Python ensurepip
+        COMMAND ${CMAKE_INSTALL_PREFIX}/python/bin/python -m ensurepip
+        DEPENDEES install
+    )
 
-    # Giving it up here.. Keeping the commented code here, just in case there will be further progress on one of the both solutions...
-    add_custom_target(Python)
-
+    SetProjectDependencies(TARGET Python DEPENDS OpenSSL)
 else()
     ExternalProject_Add(Python
         URL https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
