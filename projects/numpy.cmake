f781be4322bb7fd43281abca5231eaed96c2f226
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2017-01-18 17:20:18 -0800

    Do not specify prefix for Numpy
    The Python defaults are fine and work properly on Windows

--- projects/numpy.cmake
+++ projects/numpy.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(NumPy
     URL_MD5 2f44a895a8104ffac140c3a70edbd450
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --prefix=${CMAKE_INSTALL_PREFIX}
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
     BUILD_IN_SOURCE 1
 )
 SetProjectDependencies(TARGET NumPy DEPENDS Python OpenBLAS)
