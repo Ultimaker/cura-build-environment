5250b729e14d3930afb035ebe8fcbdc2557f7158
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2017-01-24 09:20:45 -0800

    Try and get some more stuff built

--- projects/numpy.cmake
+++ projects/numpy.cmake
@@ -1,9 +1,20 @@
-ExternalProject_Add(NumPy
-    URL http://downloads.sourceforge.net/project/numpy/NumPy/1.11.1/numpy-1.11.1.tar.gz
-    URL_MD5 2f44a895a8104ffac140c3a70edbd450
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
-    BUILD_IN_SOURCE 1
-)
-SetProjectDependencies(TARGET NumPy DEPENDS Python OpenBLAS)
+if(NOT BUILD_OS_WINDOWS)
+    ExternalProject_Add(NumPy
+        URL http://downloads.sourceforge.net/project/numpy/NumPy/1.11.1/numpy-1.11.1.tar.gz
+        URL_MD5 2f44a895a8104ffac140c3a70edbd450
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
+        INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+        BUILD_IN_SOURCE 1
+    )
+    SetProjectDependencies(TARGET NumPy DEPENDS Python OpenBLAS)
+else()
+    ExternalProject_Add(NumPy
+        GIT_REPOSITORY https://github.com/numpy/windows-wheel-builder
+        GIT_TAG origin/master
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ${CMAKE_SOURCE_DIR}/projects/build_numpy.bat ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/projects/numpy_distutils_gfortran.patch
+        INSTALL_COMMAND cd numpy/dist && ${PYTHON_EXECUTABLE} -m pip install --force-reinstall --upgrade --pre --no-index -f . numpy
+        BUILD_IN_SOURCE 1
+    )
+endif()
--- projects/openblas.cmake
+++ projects/openblas.cmake
@@ -11,26 +11,32 @@ if(BUILD_OS_LINUX)
         INSTALL_COMMAND make PREFIX=${CMAKE_INSTALL_PREFIX} ${openblas_options} install
         BUILD_IN_SOURCE 1
     )
-elseif(BUILD_OS_WINDOWS)
+# elseif(BUILD_OS_WINDOWS)
 #     enable_language(Fortran)
 #     set(openblas_options DYNAMIC_ARCH=1 NO_STATIC=1)
 
-    ExternalProject_Add(OpenBLAS
+#     ExternalProject_Add(OpenBLAS
 #         URL https://github.com/xianyi/OpenBLAS/archive/v0.2.15.tar.gz
         #URL_MD5 b1190f3d3471685f17cfd1ec1d252ac9
 #         URL https://github.com/xianyi/OpenBLAS/archive/v0.2.19.tar.gz
-        GIT_REPOSITORY https://github.com/xianyi/OpenBLAS
-        GIT_TAG origin/develop
-        PATCH_COMMAND git revert --no-commit --no-edit 9dbdc7b2
-        CMAKE_GENERATOR "Visual Studio 14 2015"
-        CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DDYNAMIC_ARCH=1 # -DBUILD_WITHOUT_LAPACK=OFF
+#         CMAKE_GENERATOR "MinGW Makefiles"
+#         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} # -DDYNAMIC_ARCH=1 # -DBUILD_WITHOUT_LAPACK=OFF
+#         GIT_REPOSITORY https://github.com/xianyi/OpenBLAS
+#         GIT_TAG 596ead0
+#         PATCH_COMMAND git revert --no-commit --no-edit 9dbdc7b2
+        # Workaround: The f_check CMake file does "CMAKE_FORCE_Fortran_COMPILER" which resets our manually set Fortran compiler
+#         PATCH_COMMAND findstr /V CMAKE_FORCE_Fortran_COMPILER "cmake\\f_check.cmake" > "cmake\\f_check.cmake.new" && move /Y "cmake\\f_check.cmake.new" "cmake\\f_check.cmake"
+#         CMAKE_GENERATOR "Visual Studio 14 2015"
+#         CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER} -DBUILD_WITHOUT_LAPACK=OFF # -DDYNAMIC_ARCH=1 # -DBUILD_WITHOUT_LAPACK=OFF
 #         BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config Release
 #         INSTALL_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config Release --target INSTALL
 #         CONFIGURE_COMMAND ""
 #         BUILD_COMMAND make ${openblas_options}
 #         INSTALL_COMMAND make PREFIX=${CMAKE_INSTALL_PREFIX} ${openblas_options} install
+#         BUILD_COMMAND mingw32-make
+#         INSTALL_COMMAND ""
 #         BUILD_IN_SOURCE 1
-    )
+#     )
 else()
     add_custom_target(OpenBLAS)
 endif()
