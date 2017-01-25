c224cebc24ce1130611042920ee596dfef2e053a
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2017-01-25 15:51:33 -0800

    Install numpy/scipy from binary packages
    It turns out building scipy is just not possible currently on Windows
    in combination with MSVC, unless you have access to ifort. Even using
    mingw is nearly impossible and has the problem of not working properly
    for 64-bit.

--- projects/numpy.cmake
+++ projects/numpy.cmake
@@ -7,14 +7,33 @@ if(NOT BUILD_OS_WINDOWS)
         INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
         BUILD_IN_SOURCE 1
     )
-    SetProjectDependencies(TARGET NumPy DEPENDS Python OpenBLAS)
 else()
+    ### MASSSIVE HACK TIME!!!!
+    # It is currently effectively impossible to build SciPy on Windows without a proprietary compiler (ifort).
+    # This means we need to use a pre-compiled binary version of Scipy. Since the only version of SciPy for
+    # Windows available depends on numpy with MKL, we also need the binary package for that.
+    if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+        set(arch_dir "win-64")
+    else()
+        set(arch_dir "win-32")
+    endif()
+
     ExternalProject_Add(NumPy
-        GIT_REPOSITORY https://github.com/numpy/windows-wheel-builder
-        GIT_TAG origin/master
+        URL https://repo.continuum.io/pkgs/free/${arch_dir}/numpy-1.11.3-py35_0.tar.bz2
         CONFIGURE_COMMAND ""
-        BUILD_COMMAND ${CMAKE_SOURCE_DIR}/projects/build_numpy.bat ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/projects/numpy_distutils_gfortran.patch
-        INSTALL_COMMAND cd numpy/dist && ${PYTHON_EXECUTABLE} -m pip install --force-reinstall --upgrade --pre --no-index -f . numpy
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory Lib/site-packages ${CMAKE_INSTALL_PREFIX}/Lib/site-packages
+        BUILD_IN_SOURCE 1
+    )
+
+    ExternalProject_Add(MKL
+        URL https://repo.continuum.io/pkgs/free/${arch_dir}/mkl-2017.0.1-0.tar.bz2
+        DEPENDS NumPy
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory Library/bin ${CMAKE_INSTALL_PREFIX}/Lib/site-packages/numpy/core
         BUILD_IN_SOURCE 1
     )
 endif()
+
+SetProjectDependencies(TARGET NumPy DEPENDS Python OpenBLAS)
