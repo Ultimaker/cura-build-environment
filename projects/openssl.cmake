0ad172e17132af53a05073a5202963e15b2835a5
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2017-02-16 13:59:06 +0100

    Fix building OpenSSL on 64-bit windows

--- projects/openssl.cmake
+++ projects/openssl.cmake
@@ -1,15 +1,17 @@
 if(BUILD_OS_WINDOWS)
     if(BUILD_OS_WIN32)
         set(_openssl_os "VC-WIN32")
+        set(_openssl_build ms\\do_ms.bat && nmake -f ms\\nt.mak)
     else()
         set(_openssl_os "VC-WIN64A")
+        set(_openssl_build ms\\do_win64a.bat && nmake -f ms\\nt.mak)
     endif()
 
     ExternalProject_Add(OpenSSL
         URL https://www.openssl.org/source/openssl-1.0.1u.tar.gz
         URL_HASH SHA256=4312b4ca1215b6f2c97007503d80db80d5157f76f8f7d3febbe6b4c56ff26739
         CONFIGURE_COMMAND perl Configure ${_openssl_os} --prefix=${CMAKE_INSTALL_PREFIX}
-        BUILD_COMMAND ms\\do_ms.bat && nmake -f ms\\nt.mak
+        BUILD_COMMAND ${_openssl_build}
         INSTALL_COMMAND nmake -f ms\\nt.mak install
         BUILD_IN_SOURCE 1
     )
