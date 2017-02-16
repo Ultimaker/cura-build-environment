e0e8cc4a62e76417d075b0dffee4ab9aa41ddc32
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2017-02-16 14:10:41 +0100

    Upgrade OpenSSL to 1.0.2

--- projects/openssl.cmake
+++ projects/openssl.cmake
@@ -8,8 +8,8 @@ if(BUILD_OS_WINDOWS)
     endif()
 
     ExternalProject_Add(OpenSSL
-        URL https://www.openssl.org/source/openssl-1.0.1u.tar.gz
-        URL_HASH SHA256=4312b4ca1215b6f2c97007503d80db80d5157f76f8f7d3febbe6b4c56ff26739
+        URL https://www.openssl.org/source/openssl-1.0.2k.tar.gz
+        URL_HASH SHA256=6b3977c61f2aedf0f96367dcfb5c6e578cf37e7b8d913b4ecb6643c3cb88d8c0
         CONFIGURE_COMMAND perl Configure ${_openssl_os} --prefix=${CMAKE_INSTALL_PREFIX}
         BUILD_COMMAND ${_openssl_build}
         INSTALL_COMMAND nmake -f ms\\nt.mak install
