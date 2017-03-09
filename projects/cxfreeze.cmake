9ea2e74b2792d3891122cecb42e2872c817dba5f
Arjen Hiemstra <ahiemstra@heimr.nl>
2017-03-09 15:26:40 +0100

    Only patch cxFreeze on OSX
    Avoids needing the "patch" executable on other platforms

--- projects/cxfreeze.cmake
+++ projects/cxfreeze.cmake
@@ -1,7 +1,11 @@
+set(_patch_command "")
+if(BUILD_OS_OSX)
+    set(_patch_command patch cx_Freeze/freezer.py ${CMAKE_SOURCE_DIR}/projects/cxfreeze_exclude_a.patch)
+endif()
 
 ExternalProject_Add(cx_Freeze
     URL https://github.com/anthony-tuininga/cx_Freeze/archive/5.0.tar.gz
-    PATCH_COMMAND patch cx_Freeze/freezer.py ${CMAKE_SOURCE_DIR}/projects/cxfreeze_exclude_a.patch
+    PATCH_COMMAND ${_patch_command}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
     INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=cxfreeze-install.log
