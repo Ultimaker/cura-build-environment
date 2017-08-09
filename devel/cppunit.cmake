4793979d3fb7cf9b1a35a190096f02ff37aa2a2c
Ghostkeeper <rubend@tutanota.com>
2017-08-09 08:48:41 +0200

    Remove invalid configure command
    On my computer it needs this extra parameter, but it doesn't work this way in CMake... How does it work?

--- devel/cppunit.cmake
+++ devel/cppunit.cmake
@@ -1,7 +1,6 @@
 ExternalProject_Add(CppUnit
     URL https://sourceforge.net/projects/cppunit/files/cppunit/1.12.1/cppunit-1.12.1.tar.gz
     URL_MD5 bd30e9cf5523cdfc019b94f5e1d7fd19
-    CONFIGURE_COMMAND "configure LDFLAGS=\"-ldl\""
     BUILD_COMMAND make
     INSTALL_COMMAND make PREFIX=${CMAKE_INSTALL_PREFIX} install
     BUILD_IN_SOURCE 1
