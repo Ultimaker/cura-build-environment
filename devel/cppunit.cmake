6951eb419d45d1a21c29062a5c6be21893940e52
Ghostkeeper <rubend@tutanota.com>
2017-08-09 10:57:56 +0200

    Various fixes for building CppUnit on Unix

--- devel/cppunit.cmake
+++ devel/cppunit.cmake
@@ -1,9 +1,8 @@
-ExternalProject_Add(CppUnit
-    URL https://sourceforge.net/projects/cppunit/files/cppunit/1.12.1/cppunit-1.12.1.tar.gz
-    URL_MD5 bd30e9cf5523cdfc019b94f5e1d7fd19
-    BUILD_COMMAND make
-    INSTALL_COMMAND make PREFIX=${CMAKE_INSTALL_PREFIX} install
-    BUILD_IN_SOURCE 1
-)
-
-SetProjectDependencies(TARGET Astroid DEPENDS Python)
+if (NOT BUILD_OS_WINDOWS)
+    ExternalProject_Add(CppUnit
+        URL https://sourceforge.net/projects/cppunit/files/cppunit/1.12.1/cppunit-1.12.1.tar.gz
+        URL_MD5 bd30e9cf5523cdfc019b94f5e1d7fd19
+        CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-doxygen=no --enable-dot=no --enable-html-docs=no LDFLAGS=-ldl
+        BUILD_IN_SOURCE 1
+    )
+endif()
