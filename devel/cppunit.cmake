5628c1ccd10fd8e966d992bc54ca9ce6ad3a4493
Ghostkeeper <rubend@tutanota.com>
2017-08-10 10:26:46 +0200

    Take master branch of CppUnit
    We've merged the CMake script into the master branch of our fork to make it better visible.

--- devel/cppunit.cmake
+++ devel/cppunit.cmake
@@ -7,7 +7,6 @@ endif()
 
 ExternalProject_Add(CppUnit
     GIT_REPOSITORY https://github.com/Ultimaker/CppUnit.git
-    GIT_TAG cmake
     CONFIGURE_COMMAND ${CMAKE_COMMAND} ../CppUnit -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${extra_cmake_args}
     BUILD_COMMAND ${make_tool}
     INSTALL_COMMAND ${make_tool} install
