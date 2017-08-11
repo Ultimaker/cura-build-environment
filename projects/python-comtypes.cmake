20f8051a7cc9b21f07dac4973ab7639029136403
Ghostkeeper <rubend@tutanota.com>
2017-08-11 14:52:02 +0200

    Patch ComTypes with a small fix
    The fix is obtained from here: https://github.com/enthought/comtypes/commit/7d66f3e0586f5e467764d57a252e09fc4e05270c
    However we can't use that repository since it contains Python 2 code.
    
    Contributes to issue CURA-3823.

--- projects/python-comtypes.cmake
+++ projects/python-comtypes.cmake
@@ -2,6 +2,7 @@ if(BUILD_OS_WINDOWS)
     ExternalProject_Add(PythonComtypes
         URL https://pypi.python.org/packages/85/11/722b9ce6725bf8160bd8aca68b1e61bd9db422ab12dae28daa7defab2cdc/comtypes-1.1.3-2.zip
         URL_MD5 4161cb8bc283a75af85e220ad662d5af
+        PATCH_COMMAND patch comtypes/__init__.py ${CMAKE_SOURCE_DIR}/cmake/python-comtypes.patch
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
         INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=comtypes-install.log
