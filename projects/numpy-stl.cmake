eb2eefd294852a4024d37104523869785f2e4538
Jaime van Kessel <nallath@gmail.com>
2017-08-16 13:16:28 +0200

    Updated numpy-stl to 2.2.3
    CURA-4157

--- projects/numpy-stl.cmake
+++ projects/numpy-stl.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(NumpySTL
-    URL https://github.com/WoLpH/numpy-stl/archive/v1.9.0.tar.gz
-    URL_MD5 cda2531edecff4468a4bbe78c6a2833b
+    URL https://github.com/WoLpH/numpy-stl/archive/v2.2.3.tar.gz
+    URL_MD5 fbd2abc07fdbbb302a649f940f2ae4cc
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
     INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=numpystl-install.log
