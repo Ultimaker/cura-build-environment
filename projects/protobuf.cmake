a7c2108205e2d8598cd3211b29ff77ac7bcfd658
Ghostkeeper <rubend@tutanota.com>
2017-10-04 12:52:03 +0200

    Update hash of Protobuf
    Apparently Github changed its .gz functionality. This one was updated for some reason... I did a diff on the source code and it looks like nothing changed.

--- projects/protobuf.cmake
+++ projects/protobuf.cmake
@@ -8,7 +8,7 @@ endif()
 
 ExternalProject_Add(Protobuf
     URL https://github.com/google/protobuf/archive/v3.0.2.tar.gz
-    URL_MD5 845b39e4b7681a2ddfd8c7f528299fbb
+    URL_MD5 7349a7f43433d72c6d805c6ca22b7eeb
     CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_CMAKEDIR=lib/cmake/protobuf -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_CXX_FLAGS=${protobuf_cxx_flags} -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_WITH_ZLIB=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -G ${CMAKE_GENERATOR} ../Protobuf/cmake
 )
 
@@ -19,7 +19,7 @@ if(BUILD_OS_WINDOWS)
     # Since the two are ABI-incompatible we need two versions of the library...
     ExternalProject_Add(Protobuf-MinGW
         URL https://github.com/google/protobuf/archive/v3.0.2.tar.gz
-        URL_MD5 845b39e4b7681a2ddfd8c7f528299fbb
+        URL_MD5 7349a7f43433d72c6d805c6ca22b7eeb
         DEPENDS Protobuf
         CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib-mingw -DCMAKE_INSTALL_BINDIR=lib-mingw -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_CXX_FLAGS="--std=c++11" -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_WITH_ZLIB=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -G "MinGW Makefiles" ../Protobuf-MinGW/cmake
         BUILD_COMMAND mingw32-make
