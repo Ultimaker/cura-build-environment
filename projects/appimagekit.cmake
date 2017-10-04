403ff911344a31cc13086ee10f648d49172a5c1b
Ghostkeeper <rubend@tutanota.com>
2017-10-04 13:03:17 +0200

    Revert "Update AppImageKit"
    This reverts commit efc0654a9567d206d2c518ece53c6da8281739cb. The change didn't work because the shell script tries to fetch subrepositories with Git but since it's extracted from a .tar.gz download this is no Git repository.

--- projects/appimagekit.cmake
+++ projects/appimagekit.cmake
@@ -2,7 +2,7 @@ if(BUILD_OS_LINUX)
     ExternalProject_Add(AppImageKit
         #GIT_REPOSITORY https://github.com/probonopd/AppImageKit.git
         #GIT_TAG origin/master
-        URL https://github.com/probonopd/AppImageKit/archive/9.tar.gz
+        URL https://github.com/probonopd/AppImageKit/archive/6.tar.gz
         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
         INSTALL_COMMAND ""
         BUILD_IN_SOURCE 1
