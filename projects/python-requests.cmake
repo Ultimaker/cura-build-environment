840c97e81f3d8e55880b70e16c4b9c6f4675f2c9
Lipu Fei <lipu.fei815@gmail.com>
2017-10-31 09:33:31 +0100

    Use a separate log file for requests installation
    CURA-4415

--- projects/python-requests.cmake
+++ projects/python-requests.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(PythonRequests
     URL_MD5 ee668fdec015db44b3a85ae8c877d3d7
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=zeroconf-install.log
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=requests-install.log
     BUILD_IN_SOURCE 1
 )
 SetProjectDependencies(TARGET PythonRequests DEPENDS Python)
