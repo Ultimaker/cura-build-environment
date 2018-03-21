7f00f7ccff7f7979186eecf03dfbfe2fb4d97ed7
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2018-03-21 17:36:38 +0100

    AppImagekit: .. and make the downloaded files executable

--- projects/appimagekit.cmake
+++ projects/appimagekit.cmake
@@ -6,9 +6,9 @@ if(BUILD_OS_LINUX)
         DEPENDS CMakeMinimal wget
         COMMENT "Installing AppImageKit tools to ${CMAKE_INSTALL_PREFIX}/bin/"
         COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/bin/
-        COMMAND wget -O ${CMAKE_INSTALL_PREFIX}/bin/appimaged ${_appimagekit_baseurl}/appimaged-x86_64.AppImage
-        COMMAND wget -O ${CMAKE_INSTALL_PREFIX}/bin/appimagetool ${_appimagekit_baseurl}/appimagetool-x86_64.AppImage
-        COMMAND wget -O ${CMAKE_INSTALL_PREFIX}/bin/AppRun ${_appimagekit_baseurl}/AppRun-x86_64
-        COMMAND wget -O ${CMAKE_INSTALL_PREFIX}/bin/runtime ${_appimagekit_baseurl}/runtime-x86_64
+        COMMAND wget -O ${CMAKE_INSTALL_PREFIX}/bin/appimaged ${_appimagekit_baseurl}/appimaged-x86_64.AppImage && chmod a+x ${CMAKE_INSTALL_PREFIX}/bin/appimaged
+        COMMAND wget -O ${CMAKE_INSTALL_PREFIX}/bin/appimagetool ${_appimagekit_baseurl}/appimagetool-x86_64.AppImage && chmod a+x ${CMAKE_INSTALL_PREFIX}/bin/appimagetool
+        COMMAND wget -O ${CMAKE_INSTALL_PREFIX}/bin/AppRun ${_appimagekit_baseurl}/AppRun-x86_64 && chmod a+x ${CMAKE_INSTALL_PREFIX}/bin/AppRun
+        COMMAND wget -O ${CMAKE_INSTALL_PREFIX}/bin/runtime ${_appimagekit_baseurl}/runtime-x86_64 && chmod a+x ${CMAKE_INSTALL_PREFIX}/bin/runtime
     )
 endif()
