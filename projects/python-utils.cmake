dc16e18826d5782c3d4c85669be6acf8edc49293
Lipu Fei <lipu.fei815@gmail.com>
2018-05-02 13:56:48 +0200

    Update python-utils to 2.3.0

--- projects/python-utils.cmake
+++ projects/python-utils.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(python-utils
-    URL https://pypi.python.org/packages/24/f6/26474b0b52335864cf43a969987b7ab27ee14331def6ec82cbb8263bc937/python-utils-2.0.0.tar.gz
-    URL_MD5 05d5fba0675e0ba3e3bf367e254d5369
+    URL https://files.pythonhosted.org/packages/30/95/d01fbd09ced38a16b7357a1d6cefe1327b9273885bffd6371cbec3e23af7/python-utils-2.3.0.tar.gz
+    URL_HASH SHA256=34aaf26b39b0b86628008f2ae0ac001b30e7986a8d303b61e1357dfcdad4f6d3
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
     INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=pythonutils-install.log
