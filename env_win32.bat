4b8f6dbeec371c0aeed4fdcd757a6e8d9711cd63
Lipu Fei <lipu.fei815@gmail.com>
2018-05-02 17:01:28 +0200

    Add CRYPTOGRAPHY_WINDOWS_LINK_LEGACY_OPENSSL for windows batch scripts

--- env_win32.bat
+++ env_win32.bat
@@ -1,1 +1,5 @@
 call "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
+REM cryptography by default links to OpenSSL 1.1.0 which has different library
+REM file names, so we need this flag to be able to link to OpenSSL 1.0.2
+set CRYPTOGRAPHY_WINDOWS_LINK_LEGACY_OPENSSL=1
--- env_win64.bat
+++ env_win64.bat
@@ -2,3 +2,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd6
 set CFLAGS=-m64
 set CXXFLAGS=-m64
 set LDFLAGS=-m64
+
+REM cryptography by default links to OpenSSL 1.1.0 which has different library
+REM file names, so we need this flag to be able to link to OpenSSL 1.0.2
+set CRYPTOGRAPHY_WINDOWS_LINK_LEGACY_OPENSSL=1
