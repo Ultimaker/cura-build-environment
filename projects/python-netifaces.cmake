509aaf2316147c1ca93f4e69e7b451625a69928b
Lipu Fei <lipu.fei815@gmail.com>
2018-05-02 13:58:57 +0200

    Update netifaces to 0.10.7

--- projects/python-netifaces.cmake
+++ projects/python-netifaces.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(PythonNetifaces
-    URL https://pypi.python.org/packages/18/fa/dd13d4910aea339c0bb87d2b3838d8fd923c11869b1f6e741dbd0ff3bc00/netifaces-0.10.4.tar.gz
-    URL_MD5 36da76e2cfadd24cc7510c2c0012eb1e
+    URL https://files.pythonhosted.org/packages/81/39/4e9a026265ba944ddf1fea176dbb29e0fe50c43717ba4fcf3646d099fe38/netifaces-0.10.7.tar.gz
+    URL_HASH SHA256=bd590fcb75421537d4149825e1e63cca225fd47dad861710c46bd1cb329d8cbd
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
     INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=netifaces-install.log
