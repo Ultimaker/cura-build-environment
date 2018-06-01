eceef368b282ecc357be40f1bf1cf133ff0f0038
Lipu Fei <lipu.fei815@gmail.com>
2018-06-01 12:12:59 +0200

    Downgrade zeroconf to 0.17.6 see if it goes crazy on Mac
    Zeroconf 0.20.0 is super busy on Mac. Let's downgrade it and see that
    still happens.

--- projects/python-zeroconf.cmake
+++ projects/python-zeroconf.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(PythonZeroconf
-    URL https://github.com/jstasiak/python-zeroconf/archive/0.20.0.tar.gz
-    URL_MD5 7ef1d9e48a2b3b5d1abe77f0e5ce2197
+    URL https://github.com/jstasiak/python-zeroconf/archive/0.17.6.tar.gz
+    URL_MD5 b48a65baede6ba78d276ff32a13a0671
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
     INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=zeroconf-install.log
