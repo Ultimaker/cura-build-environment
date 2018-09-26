8473b3485ef2c950827b0b3791e042d55032abdb
Lipu Fei <lipu.fei815@gmail.com>
2018-09-26 14:32:44 +0200

    Fix OS X flags

--- env_osx.sh
+++ env_osx.sh
@@ -3,7 +3,7 @@
 export MACOSX_DEPLOYMENT_TARGET=10.7
 export CC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
 export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
-export CFLAGS="-I/usr/local/opt/openssl/include -stdlib=libc++"
-export CXXFLAGS="-I/usr/local/opt/openssl/include -stdlib=libc++"
-export LDFLAGS="-L/usr/local/opt/openssl/lib -stdlib=libc++"
+export CFLAGS="-I/usr/local/opt/openssl/include"
+export CXXFLAGS="-I/usr/local/opt/openssl/include"
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
 
