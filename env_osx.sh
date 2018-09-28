15fa61668526b04a920ebdd76ceffb72b7de941b
Lipu Fei <lipu.fei815@gmail.com>
2018-09-28 10:07:46 +0200

    WIP: Use PATHs to specific compile-time paths

--- env_osx.sh
+++ env_osx.sh
@@ -3,7 +3,6 @@
 export MACOSX_DEPLOYMENT_TARGET=10.7
 export CC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
 export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
-export CFLAGS="-I/usr/local/opt/openssl/include"
-export CXXFLAGS="-I/usr/local/opt/openssl/include"
-export LDFLAGS="-L/usr/local/opt/openssl/lib"
-
+export CPATH="/usr/local/opt/openssl/include:/usr/local/opt/sqlite/include:$CPATH"
+export LIBRARY_PATH="/usr/local/opt/openssl/lib:/usr/local/opt/sqlite/lib:$LIBRARY_PATH"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/sqlite/lib/pkgconfig:$PKG_CONFIG_PATH"
