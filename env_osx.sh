a0f8bd5d3f48767adeff7148c28827595b45d93e
Lipu Fei <lipu.fei815@gmail.com>
2018-10-02 12:15:05 +0200

    Fix libc++ reference on Mac

--- env_osx.sh
+++ env_osx.sh
@@ -6,3 +6,4 @@ export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xc
 export CPATH="/usr/local/opt/openssl/include:/usr/local/opt/sqlite/include:$CPATH"
 export LIBRARY_PATH="/usr/local/opt/openssl/lib:/usr/local/opt/sqlite/lib:$LIBRARY_PATH"
 export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/sqlite/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CXXFLAGS="-stdlib=libc++"
