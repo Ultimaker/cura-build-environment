2f4090e724ce6f1a02d55906a218f94f61faa0fa
Lipu Fei <lipu.fei815@gmail.com>
2018-10-18 09:20:07 +0200

    Add missing files
    CURA-5809
    CURA-5812

---
+++ projects/xz.cmake
@@ -0,0 +1,10 @@
+if(BUILD_OS_OSX)
+    ExternalProject_Add(xz
+        URL https://sourceforge.net/projects/lzmautils/files/xz-5.2.4.tar.gz
+        URL_HASH SHA256=b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145
+        CONFIGURE_COMMAND ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX} --with-sysroot=${CMAKE_OSX_SYSROOT}
+        BUILD_COMMAND make
+        INSTALL_COMMAND make install
+        BUILD_IN_SOURCE 1
+    )
+endif()
