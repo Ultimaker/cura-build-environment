441d7104c5ea563950034dc0cdf4e231e1cb2195
Lipu Fei <lipu.fei815@gmail.com>
2019-01-18 14:37:17 +0100

    Add lxml
    CURA-6128

--- projects/lxml.cmake
+++ projects/lxml.cmake
@@ -1,11 +1,10 @@
-add_custom_target(install_lxml)
-
-add_custom_command(
-    TARGET install_lxml PRE_BUILD
+add_custom_target(lxml
     COMMAND ${PYTHON_EXECUTABLE} -m pip install lxml==4.3.0
     COMMENT "Install lxml"
 )
 
 if(NOT BUILD_OS_WINDOWS)
-    add_dependencies(install_lxml libxslt libxml2 Python)
+    SetProjectDependencies(TARGET lxml DEPENDS libxslt libxml2 Python)
+else()
+    SetProjectDependencies(TARGET lxml DEPENDS Python)
 endif()
