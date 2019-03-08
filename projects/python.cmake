7e50672c02280543fd307676b1f31a2dc8e95299
Ghostkeeper <rubend@tutanota.com>
2019-03-08 15:57:42 +0100

    Revert "Add sqlite3"
    It doesn't build properly yet.
    
    This reverts commit 74f703dc3992c8d67f9fa306b6747d178436431e.

--- projects/python.cmake
+++ projects/python.cmake
@@ -46,9 +46,9 @@ ExternalProject_Add(Python
 
 # Only build geos on Linux
 if(BUILD_OS_LINUX)
-    SetProjectDependencies(TARGET Python DEPENDS OpenBLAS Geos xz sqlite3)
+    SetProjectDependencies(TARGET Python DEPENDS OpenBLAS Geos xz)
 elseif(BUILD_OS_OSX)
-    SetProjectDependencies(TARGET Python DEPENDS OpenBLAS Geos OpenSSL xz sqlite3)
+    SetProjectDependencies(TARGET Python DEPENDS OpenBLAS Geos OpenSSL xz)
 else()
     SetProjectDependencies(TARGET Python DEPENDS OpenBLAS)
 endif()
