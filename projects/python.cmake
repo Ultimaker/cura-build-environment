e0be043787abef750914a8a37c9c4ca758046090
Lipu Fei <lipu.fei815@gmail.com>
2019-04-05 10:57:53 +0200

    Update zeroconf to 0.17.7
    CURA-6163

--- projects/python.cmake
+++ projects/python.cmake
@@ -150,7 +150,7 @@ ExternalProject_Add_Step(Python add_other_python_packages
     COMMAND ${PYTHON_EXECUTABLE} -m pip install typing==3.6.6
     COMMAND ${PYTHON_EXECUTABLE} -m pip install urllib3==1.24.1
     COMMAND ${PYTHON_EXECUTABLE} -m pip install PyYAML==3.13
-    COMMAND ${PYTHON_EXECUTABLE} -m pip install zeroconf==0.17.6
+    COMMAND ${PYTHON_EXECUTABLE} -m pip install zeroconf==0.17.7
     DEPENDEES add_numpy_scipy_shapely
 )
 
