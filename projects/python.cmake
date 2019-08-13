04ff836205960057b03fee13b40e484af1aac683
Lipu Fei <lipu.fei815@gmail.com>
2019-08-13 08:00:05 +0200

    Update Python to 3.5.7

--- projects/python.cmake
+++ projects/python.cmake
@@ -35,8 +35,8 @@ if(BUILD_OS_WINDOWS)
 endif()
 
 ExternalProject_Add(Python
-    URL https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
-    URL_MD5 3fe8434643a78630c61c6464fe2e7e72
+    URL https://www.python.org/ftp/python/3.5.7/Python-3.5.7.tgz
+    URL_MD5 92f4c16c55429bf986f5ab45fe3a6659
     PATCH_COMMAND ${python_patch_command}
     CONFIGURE_COMMAND "${python_configure_command}"
     BUILD_COMMAND ${python_build_command}
