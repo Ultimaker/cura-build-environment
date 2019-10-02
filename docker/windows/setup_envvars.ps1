3b8aa7ef01defcaa4d478cb9ad330bd0caadb471
Lipu Fei <lipu.fei815@gmail.com>
2019-10-02 09:46:09 +0200

    WIP: Add wixtoolset

--- docker/windows/setup_envvars.ps1
+++ docker/windows/setup_envvars.ps1
@@ -1,6 +1,7 @@
 # Set up environment variables for the cura-build-environment docker image.
 
 # chocolatey doesn't seem to add NSIS to PATH, so we add it here.
+[Environment]::SetEnvironmentVariable("Path", "$env:Path" + ";C:\Program Files (x86)\wixtoolset", [System.EnvironmentVariableTarget]::Machine)
 [Environment]::SetEnvironmentVariable("Path", "$env:Path" + ";C:\Program Files (x86)\NSIS", [System.EnvironmentVariableTarget]::Machine)
 [Environment]::SetEnvironmentVariable("Path", "$env:Path" + ";C:\Program Files (x86)\Poedit\GettextTools\bin", [System.EnvironmentVariableTarget]::Machine)
 
