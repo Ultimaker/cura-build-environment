803a76ffd6e9e0fb9301caf527c4ea0ab8a7a8dc
Lipu Fei <lipu.fei815@gmail.com>
2020-01-02 16:49:31 +0100

    Add twisted 19.10.0

--- projects/python_packages.cmake
+++ projects/python_packages.cmake
@@ -85,6 +85,8 @@ add_custom_target(PythonPackages ALL
     # https://github.com/mikedh/trimesh/issues/575 since 3.2.34
     COMMAND ${Python3_EXECUTABLE} -m pip install trimesh==3.2.33
     COMMAND ${Python3_EXECUTABLE} -m pip install typing==3.7.4
+    # For testing HTTP requests
+    COMMAND ${Python3_EXECUTABLE} -m pip install twisted==19.10.0
     COMMAND ${Python3_EXECUTABLE} -m pip install urllib3==1.25.6
     COMMAND ${Python3_EXECUTABLE} -m pip install PyYAML==5.1.2
     COMMAND ${Python3_EXECUTABLE} -m pip install zeroconf==0.24.1
