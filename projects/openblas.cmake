8168c52e019c21b9c24678a2a464f9fd63050d0e
Lipu Fei <lipu.fei815@gmail.com>
2020-01-27 15:53:19 +0100

    WIP: Fix binary compatibility of OpenBLAS

--- projects/openblas.cmake
+++ projects/openblas.cmake
@@ -1,11 +1,11 @@
 if(NOT BUILD_OS_WINDOWS)
     # Fortran compiler is needed for OpenBLAS, but it does no check whether it is available.
     enable_language(Fortran)
-    set(openblas_options DYNAMIC_ARCH=1 NO_STATIC=1)
+    set(openblas_options DYNAMIC_ARCH=1 NO_STATIC=1 TARGET=HASWELL)
 
     ExternalProject_Add(OpenBLAS
-        URL https://github.com/xianyi/OpenBLAS/archive/v0.3.5.tar.gz
-        URL_MD5 579bda57f68ea6e9074bf5780e8620bb
+        URL https://github.com/xianyi/OpenBLAS/archive/v0.3.7.tar.gz
+        URL_MD5 5cd4ff3891b66a59e47af2d14cde4056
         CONFIGURE_COMMAND ""
         BUILD_COMMAND make PREFIX=${CMAKE_INSTALL_PREFIX} ${openblas_options}
         INSTALL_COMMAND make PREFIX=${CMAKE_INSTALL_PREFIX} ${openblas_options} install
