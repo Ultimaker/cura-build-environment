9f604b542ba1de1b258cc7c7de4a533046b6d2d4
evtrados <evtrados@gmail.com>
2020-04-06 21:09:02 +0200

    Updating the download URL of OpenSSL.

--- projects/openssl.cmake
+++ projects/openssl.cmake
@@ -3,7 +3,7 @@ if(BUILD_OS_OSX)
     set(_openssl_args no-ssl2 no-ssl3 no-zlib shared enable-cms)
 
     ExternalProject_Add(OpenSSL
-        URL https://www.openssl.org/source/openssl-1.0.2p.tar.gz
+        URL https://www.openssl.org/source/old/1.0.2/openssl-1.0.2p.tar.gz
         URL_HASH SHA256=50a98e07b1a89eb8f6a99477f262df71c6fa7bef77df4dc83025a2845c827d00
         CONFIGURE_COMMAND perl Configure --prefix=${CMAKE_INSTALL_PREFIX} --openssldir=${CMAKE_INSTALL_PREFIX} ${_openssl_args} ${_openssl_os}
         BUILD_COMMAND make depend && make
@@ -15,7 +15,7 @@ elseif(BUILD_OS_LINUX)
     set(_openssl_args no-ssl2 no-ssl3 no-zlib shared enable-cms)
 
     ExternalProject_Add(OpenSSL
-        URL https://www.openssl.org/source/openssl-1.0.2p.tar.gz
+        URL https://www.openssl.org/source/old/1.0.2/openssl-1.0.2p.tar.gz
         URL_HASH SHA256=50a98e07b1a89eb8f6a99477f262df71c6fa7bef77df4dc83025a2845c827d00
         CONFIGURE_COMMAND perl Configure --prefix=${CMAKE_INSTALL_PREFIX} --openssldir=${CMAKE_INSTALL_PREFIX} ${_openssl_args} ${_openssl_os}
         BUILD_COMMAND make depend && make
@@ -36,7 +36,7 @@ if(BUILD_OS_WINDOWS)
     endif()
 
     ExternalProject_Add(OpenSSL
-        URL https://www.openssl.org/source/openssl-1.0.2k.tar.gz
+        URL https://www.openssl.org/source/old/1.0.2/openssl-1.0.2k.tar.gz
         URL_HASH SHA256=6b3977c61f2aedf0f96367dcfb5c6e578cf37e7b8d913b4ecb6643c3cb88d8c0
         CONFIGURE_COMMAND perl Configure ${_openssl_os} --prefix=${CMAKE_INSTALL_PREFIX}
         BUILD_COMMAND ${_openssl_build}
