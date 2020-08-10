7f2e2c9b974cae99894da689bbe3569b0c55ca5b
Ghostkeeper <rubend@tutanota.com>
2020-08-10 17:03:57 +0200

    Add headers of boost to build environment
    The source code is simply copied to the include directories. That should allow the header-only libraries to be used.
    Not sure if there is a better way to do this. There doesn't seem to be a built-in way with FindBoost or anything. That simply looks for the libraries.

---
+++ projects/boost_headers.cmake
@@ -0,0 +1,8 @@
+ExternalProject_Add(BoostHeaders
+    URL http://sourceforge.net/projects/boost/files/boost/1.67.0/boost_1_67_0.tar.bz2
+    URL_HASH SHA1=694ae3f4f899d1a80eb7a3b31b33be73c423c1ae
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND "${CMAKE_COMMAND}" -E copy_directory "${CMAKE_CURRENT_BINARY_DIR}/BoostHeaders-prefix/src/BoostHeaders/boost" "${CMAKE_INSTALL_PREFIX}/include"
+)
+
