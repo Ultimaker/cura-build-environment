1f02271e03dd5084922f87e7ec6a259f28d49bd7
Ghostkeeper <rubend@tutanota.com>
2020-08-14 14:13:22 +0200

    Put boost headers in a subdirectory
    The Boost CMake script expects to find a file called Boost_INCLUDE_DIR/boost/config.hpp. Instead we created a file in Boost_INCLUDE_DIR/config.hpp. Oops.
    
    Contributes to issue CURA-7565.

--- projects/boost_headers.cmake
+++ projects/boost_headers.cmake
@@ -3,6 +3,6 @@ ExternalProject_Add(BoostHeaders
     URL_HASH SHA1=694ae3f4f899d1a80eb7a3b31b33be73c423c1ae
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND "${CMAKE_COMMAND}" -E copy_directory "${CMAKE_CURRENT_BINARY_DIR}/BoostHeaders-prefix/src/BoostHeaders/boost" "${CMAKE_INSTALL_PREFIX}/include"
+    INSTALL_COMMAND "${CMAKE_COMMAND}" -E copy_directory "${CMAKE_CURRENT_BINARY_DIR}/BoostHeaders-prefix/src/BoostHeaders/boost" "${CMAKE_INSTALL_PREFIX}/include/boost"
 )
 
