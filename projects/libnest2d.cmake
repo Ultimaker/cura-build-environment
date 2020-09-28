39298d203d115b60d7093f0a801be1bad0ba7842
Ghostkeeper <rubend@tutanota.com>
2020-09-28 19:00:02 +0200

    Add instructions for building libnest2d
    This works except that it doesn't like the dependency on NLopt for some reason. I'm getting an error which I have no idea on how to solve, that it can't find the nlopt target.
    
    Contributes to issue CURA-7259.

--- projects/libnest2d.cmake
+++ projects/libnest2d.cmake
@@ -1,11 +1,12 @@
 #Copyright (c) 2020 Ultimaker B.V.
 #cura-build-environment is released under the terms of the AGPLv3 or higher.
 
-#NLopt (dependency of libnest2d).
-ExternalProject_Add(nlopt
-    GIT_REPOSITORY https://github.com/stevengj/nlopt.git
-    GIT_TAG v2.6.2
+#libnest2d (dependency of pynest2d).
+ExternalProject_Add(libnest2d
+    GIT_REPOSITORY https://github.com/tamasmeszaros/libnest2d.git
+    GIT_TAG da4782500da4eb8cb6e38e5e3f10164ec5a59778 #First tag with LGPL license.
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+    DEPENDS BoostHeaders nlopt Clipper
 )
