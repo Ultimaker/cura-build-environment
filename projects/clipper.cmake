bb4086c28f6fbbdc82363b5d51a7252b9f503ee5
Ghostkeeper <rubend@tutanota.com>
2020-09-30 12:11:07 +0200

    Add patch to fix installation directories on Windows
    On Windows the installation directories were incomplete as it didn't find a library directory. This adds 'include(GNUInstallDirs)' which hopefully resolves the problem.
    This is done as a complete replacement of the CMakeLists.txt file since Windows has no patch executable that applies unified diff files.
    
    Contributes to issue CURA-7259.

--- projects/clipper.cmake
+++ projects/clipper.cmake
@@ -8,7 +8,7 @@ if(NOT TARGET Clipper)
     ExternalProject_Add(Clipper
         URL https://sourceforge.net/projects/polyclipping/files/clipper_ver6.4.2.zip
         URL_HASH SHA1=b05c1f454c22576f867fc633b11337d053e9ea33
-        PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/cpp" "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper"
+        PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/cpp" "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper" && ${CMAKE_COMMAND} -E copy "${CMAKE_SOURCE_DIR}/projects/clipper_cmakelists_patch.txt" "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/CMakeLists.txt"
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                    -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
