36777f9c26aa8bf5f0d0df80b1281b89705fae8d
Ghostkeeper <rubend@tutanota.com>
2020-09-30 09:02:38 +0200

    Use shallow copy of Git
    This saves some time and network costs.
    
    Contributes to issue CURA-7259.

--- projects/libnest2d.cmake
+++ projects/libnest2d.cmake
@@ -9,6 +9,7 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/projects/clipper.cmake)
 ExternalProject_Add(libnest2d
     GIT_REPOSITORY https://github.com/tamasmeszaros/libnest2d.git
     GIT_TAG da4782500da4eb8cb6e38e5e3f10164ec5a59778 #First tag with LGPL license.
+    GIT_SHALLOW 1
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
--- projects/nlopt.cmake
+++ projects/nlopt.cmake
@@ -6,6 +6,7 @@ if(NOT TARGET nlopt)
     ExternalProject_Add(nlopt
         GIT_REPOSITORY https://github.com/stevengj/nlopt.git
         GIT_TAG v2.6.2
+        GIT_SHALLOW 1
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                    -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
