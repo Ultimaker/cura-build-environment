1c01a4682480c4b9d1a7474f1e084c40dd8388e0
Ghostkeeper <rubend@tutanota.com>
2020-09-30 09:02:10 +0200

    Don't use SOURCE_SUBDIR
    This was introduced in CMake 3.7, but our build system is still using 3.6. Bad luck!
    
    Contributes to issue CURA-7259.

--- projects/clipper.cmake
+++ projects/clipper.cmake
@@ -6,7 +6,7 @@ if(NOT TARGET Clipper)
     ExternalProject_Add(Clipper
         URL https://sourceforge.net/projects/polyclipping/files/clipper_ver6.4.2.zip
         URL_HASH SHA1=b05c1f454c22576f867fc633b11337d053e9ea33
-        SOURCE_SUBDIR cpp
+        CMAKE_COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/cpp
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                    -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
