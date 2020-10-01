b5a38c9397c4826c51b1252b1c65f63c7384f164
Ghostkeeper <rubend@tutanota.com>
2020-10-01 09:50:03 +0200

    Add patch for finding Clipper for libnest2d on Windows
    On Windows the environment variable doesn't get set, and as a result Clipper doesn't get found. Instead we'll patch the find script in libnest2d. Quite hacky, but effective. We add that it searches also in CMAKE_INSTALL_PREFIX for the library and the headers. Other solutions (changing CMAKE_PREFIX_PATH via a -D argument, actually setting the variable with setx, etc.) were ineffective. It's weird.
    
    Contributes to issue CURA-7259.

--- projects/libnest2d.cmake
+++ projects/libnest2d.cmake
@@ -5,15 +5,12 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/projects/boost_headers.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/projects/nlopt.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/projects/clipper.cmake)
 
-# The find script in libnest2d doesn't find the clipper headers from the installation path properly. Point it out to them.
-set(ENV{CLIPPER_PATH} ${CMAKE_INSTALL_PREFIX})
-
 #libnest2d (dependency of pynest2d).
 ExternalProject_Add(libnest2d
     GIT_REPOSITORY https://github.com/tamasmeszaros/libnest2d.git
     GIT_TAG da4782500da4eb8cb6e38e5e3f10164ec5a59778 #First tag with LGPL license.
     GIT_SHALLOW 1
-    CMAKE_COMMAND ${_cmake_command_find_clipper}
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/projects/libnest2d_find_clipper.cmake" "${CMAKE_CURRENT_BINARY_DIR}/libnest2d-prefix/src/libnest2d/cmake_modules/FindClipper.cmake"
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
