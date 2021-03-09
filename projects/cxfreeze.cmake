0fc8d1634f14ddf43c0fc3d337c4c473276600ae
Evangelos Trantos <evtrados@gmail.com>
2021-03-09 11:55:52 +0100

    Updated to the latest version of cxFreeze

--- projects/cxfreeze.cmake
+++ projects/cxfreeze.cmake
@@ -1,9 +1,9 @@
 set(_patch_command "")
 if(BUILD_OS_OSX)
-    set(_patch_command patch cx_Freeze/freezer.py ${CMAKE_SOURCE_DIR}/projects/cxfreeze_exclude_a.patch)
+    #set(_patch_command patch cx_Freeze/freezer.py ${CMAKE_SOURCE_DIR}/projects/cxfreeze_exclude_a.patch)
 endif()
 
-set(_url "https://github.com/anthony-tuininga/cx_Freeze/archive/5.0.tar.gz")
+set(_url "https://github.com/anthony-tuininga/cx_Freeze/archive/6.5.3.tar.gz")
 if(BUILD_OS_WINDOWS)
     set(_url https://github.com/anthony-tuininga/cx_Freeze/archive/5.0.1.tar.gz)
 endif()
@@ -17,4 +17,3 @@ ExternalProject_Add(cx_Freeze
     BUILD_IN_SOURCE 1
 )
 SetProjectDependencies(TARGET cx_Freeze DEPENDS Python)
-
