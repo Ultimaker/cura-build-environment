bee4c623656a276669e48e08dfb52ee32e7909f2
Evangelos Trantos <evtrados@gmail.com>
2021-05-17 14:33:55 +0200

    Change the file path of vcvarsall.bat
    After the installation of Visual Studio 2019 the path to the native command line tools have changed, so we will have to update them.

--- env_win32.bat
+++ env_win32.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 
 REM cryptography by default links to OpenSSL 1.1.0 which has different library
 REM file names, so we need this flag to be able to link to OpenSSL 1.0.2
