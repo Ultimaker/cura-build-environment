b3df75d31dcce5e7835e538612fccbbc6a42c16d
Jelle Spijker <spijker.jelle@gmail.com>
2021-09-10 13:28:29 +0200

    Use SHA256 to verify PyQt5
    Moved the Windows PyQt5 dependency to the requirements.txt
    
    I believe we can use the version provided by PyPi for all OSes since
    We have already decided to drag PyPi in our circle of trusts
    
    Contributes to CURA-8403

--- projects/pyqt.cmake
+++ projects/pyqt.cmake
@@ -1,14 +1,9 @@
 set(pyqt_command "")
-if(BUILD_OS_WINDOWS)
+if(NOT BUILD_OS_WINDOWS)
     # Building PyQt on Windows is problematic due to linking against specific Windows libraries.
     # Instead, we'll use the less favourable approach of installation via Pip, which drags PyPI into the circle of trust.
-    add_custom_target(PyQt
-        COMMAND ${Python3_EXECUTABLE} -m pip install PyQt5==5.15.2
-        COMMENT "Installing PyQt5"
-    )
-
-    SetProjectDependencies(TARGET PyQt DEPENDS Python)
-else()
+    # See requirements.txt for installing on windows with pip
+    # TODO: PyPi is already in out circle of trust why not use the same approach for the Linux and MacOS
     if(BUILD_OS_OSX)
         set(pyqt_command
             "DYLD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib"
@@ -32,7 +27,7 @@ else()
 
     ExternalProject_Add(PyQt
         URL https://files.pythonhosted.org/packages/28/6c/640e3f5c734c296a7193079a86842a789edb7988dca39eab44579088a1d1/PyQt5-5.15.2.tar.gz
-        URL_MD5 b94576e9e013210dc5aba061913e4bd4
+        URL_HASH SHA256=372b08dc9321d1201e4690182697c5e7ffb2e0770e6b4a45519025134b12e4fc
         CONFIGURE_COMMAND ${pyqt_command}
         BUILD_IN_SOURCE 1
     )
