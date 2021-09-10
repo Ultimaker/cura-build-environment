3cc0f844ac9a0baf9e2cf674ddb8c6582162b8ea
Jelle Spijker <spijker.jelle@gmail.com>
2021-09-10 12:57:51 +0200

    Use SHA256 to verify GoogleTest
    Contributes to CURA-8403

--- projects/googletest.cmake
+++ projects/googletest.cmake
@@ -1,9 +1,9 @@
 set(_googletest_url "https://github.com/google/googletest/archive/release-1.8.1.tar.gz")
-set(_googletest_md5sum "2e6fbeb6a91310a16efe181886c59596")
+set(_googletest_sha256 "9bf1fe5182a604b4135edc1a425ae356c9ad15e9b23f9f12a02e80184c3a249c")
 
 ExternalProject_Add(GoogleTest
     URL ${_googletest_url}
-    URL_MD5 ${_googletest_md5sum}
+        URL_HASH SHA256=${_googletest_sha256}
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
