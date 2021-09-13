ba4a35892a02a79fd209239ed1bb0841bba07a07
Jelle Spijker <j.spijker@ultimaker.com>
2021-09-13 16:44:26 +0200

    Installing the Pyhton packages is dependend on Python
    Sigh. the esnure_pip was a step in the external_project and
    The PythonPackagesGeneral should therefore be dependend on
    Python

--- projects/python_packages.cmake
+++ projects/python_packages.cmake
@@ -1,5 +1,5 @@
 add_custom_target(PythonPackagesGeneral ALL
         COMMAND ${Python3_EXECUTABLE} -m pip install --require-hashes -r  ${CMAKE_CURRENT_SOURCE_DIR}/requirements.txt
         COMMENT "Install Python packages"
-        DEPENDS ensurepip
+        DEPENDS Python
         )
