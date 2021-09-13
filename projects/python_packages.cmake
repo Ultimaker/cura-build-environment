baebd8b43e42c2b7d22df4f26e786f32eabaf009
Jelle Spijker <j.spijker@ultimaker.com>
2021-09-13 17:21:51 +0200

    use the correct path to requirements.txt
    Contributes to CURA-8400 and CURA-8403

--- projects/python_packages.cmake
+++ projects/python_packages.cmake
@@ -1,5 +1,5 @@
 add_custom_target(PythonPackagesGeneral ALL
-        COMMAND ${Python3_EXECUTABLE} -m pip install --require-hashes -r  ${CMAKE_CURRENT_SOURCE_DIR}/requirements.txt
+        COMMAND ${Python3_EXECUTABLE} -m pip install --require-hashes -r  ${CMAKE_SOURCE_DIR}/projects/requirements.txt
         COMMENT "Install Python packages"
         DEPENDS Python
         )
