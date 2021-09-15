b34661f88b4ebec86d42174f792381b1b3cf2167
Jelle Spijker <spijker.jelle@gmail.com>
2021-09-15 15:34:05 +0200

    Depend on the Python step
    Oversight in the last commit.
    
    Contributes to issue CURA-8400 and CURA-8403

--- projects/python_packages.cmake
+++ projects/python_packages.cmake
@@ -1,5 +1,5 @@
 add_custom_target(PythonPackagesGeneral ALL
         COMMAND ${Python3_EXECUTABLE} -m pip install --require-hashes -r  ${CMAKE_SOURCE_DIR}/projects/requirements.txt
         COMMENT "Install Python packages"
-        DEPENDS PythonPackagesBase OpenSSL
+        DEPENDS Python OpenSSL
         )
