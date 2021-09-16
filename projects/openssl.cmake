4c208dff9989e29b7a866ec665d86709f7e581b5
Jelle Spijker <spijker.jelle@gmail.com>
2021-09-16 11:08:43 +0200

    Simplified openssl
    Contributes to issue CURA-8400 and CURA-8403

--- projects/openssl.cmake
+++ projects/openssl.cmake
@@ -1,46 +1,23 @@
-if(BUILD_OS_OSX)
-    set(_openssl_os darwin64-x86_64-cc enable-ec_nistp_64_gcc_128)
-    set(_openssl_args no-ssl2 no-ssl3 no-zlib shared enable-cms)
-
-    ExternalProject_Add(OpenSSL
-        URL https://www.openssl.org/source/openssl-1.1.1l.tar.gz
-        URL_HASH SHA256=0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1
-        CONFIGURE_COMMAND perl Configure --prefix=${CMAKE_INSTALL_PREFIX} --openssldir=${CMAKE_INSTALL_PREFIX} ${_openssl_args} ${_openssl_os}
-        BUILD_COMMAND make depend && make
-        INSTALL_COMMAND make install
-        BUILD_IN_SOURCE 1
-    )
-endif()
-
-if(BUILD_OS_LINUX)
-    set(_openssl_os linux-x86_64 enable-ec_nistp_64_gcc_128)
-    set(_openssl_args no-ssl2 no-ssl3 no-zlib shared enable-cms)
-
-    ExternalProject_Add(OpenSSL
-        URL https://www.openssl.org/source/openssl-1.1.1l.tar.gz
-        URL_HASH SHA256=0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1
-        CONFIGURE_COMMAND perl Configure --prefix=${CMAKE_INSTALL_PREFIX} --openssldir=${CMAKE_INSTALL_PREFIX} ${_openssl_args} ${_openssl_os}
-        BUILD_COMMAND make depend && make
-        INSTALL_COMMAND make install
-        BUILD_IN_SOURCE 1
-    )
-endif()
-
 if(BUILD_OS_WINDOWS)
-    if(BUILD_OS_WIN32)
-        set(_openssl_os "VC-WIN32")
-        set(_openssl_build ms\\do_nasm.bat && nmake -f ms\\nt.mak)
+    set(_openssl_build "ms\\do_win64a.bat && nmake -f ms\\nt.mak")
+    set(_openssl_install "nmake -f ms\\nt.mak install")
+    set(_openssl_configure "perl Configure VC-WIN64A --prefix=${CMAKE_INSTALL_PREFIX}")
+else()
+    set(_openssl_build "make depend && make")
+    set(_openssl_install "make install")
+    if(BUILD_OS_LINUX)
+        set(_openssl_configure "perl Configure --prefix=${CMAKE_INSTALL_PREFIX} --openssldir=${CMAKE_INSTALL_PREFIX} no-ssl2 no-ssl3 no-zlib shared enable-cms linux-x86_64 enable-ec_nistp_64_gcc_128")
     else()
-        set(_openssl_os "VC-WIN64A")
-        set(_openssl_build ms\\do_win64a.bat && nmake -f ms\\nt.mak)
+        set(_openssl_configure "perl Configure --prefix=${CMAKE_INSTALL_PREFIX} --openssldir=${CMAKE_INSTALL_PREFIX} no-ssl2 no-ssl3 no-zlib shared enable-cms darwin64-x86_64-cc enable-ec_nistp_64_gcc_128")
     endif()
-
-    ExternalProject_Add(OpenSSL
-        URL https://www.openssl.org/source/openssl-1.1.1l.tar.gz
-        URL_HASH SHA256=0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1
-        CONFIGURE_COMMAND perl Configure ${_openssl_os} --prefix=${CMAKE_INSTALL_PREFIX}
-        BUILD_COMMAND ${_openssl_build}
-        INSTALL_COMMAND nmake -f ms\\nt.mak install
-        BUILD_IN_SOURCE 1
-    )
 endif()
+
+ExternalProject_Add(OpenSSL
+    URL https://www.openssl.org/source/openssl-1.1.1l.tar.gz
+    URL_HASH SHA256=0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1
+    CONFIGURE_COMMAND ${_openssl_configure}
+    BUILD_COMMAND ${_openssl_build}
+    INSTALL_COMMAND ${_openssl_install}
+    BUILD_IN_SOURCE 1
+)
+
