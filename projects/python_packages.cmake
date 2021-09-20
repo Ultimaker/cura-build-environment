360683466b1e17be2865807d63a346395bee1b14
Vagos <evtrados@gmail.com>
2021-09-20 14:34:13 +0200

    Make the post-build command platform specific

--- projects/python_packages.cmake
+++ projects/python_packages.cmake
@@ -4,9 +4,11 @@ add_custom_target(PythonPackagesGeneral ALL
     DEPENDS Python
 )
 
-add_custom_command(
-    TARGET PythonPackagesGeneral POST_BUILD
-    COMMAND powershell -Command "(gc hooks.py) -replace 'libffi-7.dll', 'libffi-8.dll' | Out-File -encoding ASCII hooks.py"
-    COMMENT "Make libffi-8.dll visible to cx_Freeze"
-    WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib/site-packages/cx_Freeze
-)
+if(BUILD_OS_WINDOWS)
+    add_custom_command(
+        TARGET PythonPackagesGeneral POST_BUILD
+        COMMAND powershell -Command "(gc hooks.py) -replace 'libffi-7.dll', 'libffi-8.dll' | Out-File -encoding ASCII hooks.py"
+        COMMENT "Make libffi-8.dll visible to cx_Freeze"
+        WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib/site-packages/cx_Freeze
+    )
+endif()
