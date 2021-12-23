a533bee005fde5062fcf2cc1c61668e747319359
Ghostkeeper <rubend@tutanota.com>
2021-12-23 17:11:10 +0100

    Upgrade to CPython 3.10.0
    This causes a different source tree to be built.
    
    Contributes to issue CURA-8592.

--- projects/python.cmake
+++ projects/python.cmake
@@ -20,8 +20,8 @@ if(BUILD_OS_WINDOWS)
     set(python_patch_command ${CMAKE_COMMAND} -E copy "${CMAKE_SOURCE_DIR}/projects/python_libffi_patch.props" "${CMAKE_CURRENT_BINARY_DIR}/Python-prefix/src/Python/PCbuild/libffi.props")
 
     ExternalProject_Add(Python
-        URL https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tgz
-        URL_HASH SHA256=b37ac74d2cbad2590e7cd0dd2b3826c29afe89a734090a87bf8c03c45066cb65
+        URL https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
+        URL_HASH SHA256=c4e0cbad57c90690cb813fb4663ef670b4d0f587d8171e2c42bd4c9245bd2758
         PATCH_COMMAND ${python_patch_command}
         # The python3.8 build configuration still downloads and installs OpenSSL v1.1.1k from the cpython-bin-deps repository.
         # Thus, we have to force it to use OpenSSL v1.1.1l, as it fixes several security risks
@@ -43,8 +43,8 @@ if(BUILD_OS_LINUX)
 endif()
 
 ExternalProject_Add(Python
-    URL https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tgz
-    URL_HASH SHA256=b37ac74d2cbad2590e7cd0dd2b3826c29afe89a734090a87bf8c03c45066cb65
+    URL https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
+    URL_HASH SHA256=c4e0cbad57c90690cb813fb4663ef670b4d0f587d8171e2c42bd4c9245bd2758
     CONFIGURE_COMMAND "${python_configure_command}"
     BUILD_COMMAND ${python_build_command}
     INSTALL_COMMAND ${python_install_command}
