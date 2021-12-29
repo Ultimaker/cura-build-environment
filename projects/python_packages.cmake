e1256f4a80fa74ed0b897b5a785f426081b6af63
Ghostkeeper <rubend@tutanota.com>
2021-12-29 15:47:45 +0100

    Set linker flags instead of root
    Another attempt.
    This is taken from https://stackoverflow.com/questions/22949372/python-pip-library-search-paths , which is a similar problem to mine.
    
    Contributes to issue CURA-8592.

--- projects/python_packages.cmake
+++ projects/python_packages.cmake
@@ -1,5 +1,5 @@
 add_custom_target(PythonPackagesGeneral ALL
-    COMMAND ${Python3_EXECUTABLE} -m pip install --root=${CMAKE_INSTALL_PREFIX} --require-hashes -r  ${CMAKE_SOURCE_DIR}/projects/requirements.txt
+    COMMAND LD_FLAGS="-L${CMAKE_INSTALL_PREFIX}" ${Python3_EXECUTABLE} -m pip install --require-hashes -r  ${CMAKE_SOURCE_DIR}/projects/requirements.txt
     COMMENT "Install Python packages"
     DEPENDS Python
 )
