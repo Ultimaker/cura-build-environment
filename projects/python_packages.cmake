6a00e0419f72c93d056bfef4ea52a8c9a90629cf
Ghostkeeper <rubend@tutanota.com>
2022-03-01 18:06:31 +0100

    Delete certain Qt plug-ins after downloading them
    These cause the builds to fail if they are present.
    Let's hope we don't need them. This hasn't yet been properly tested at this point since the build segfaults on something else.
    The NativeStyle hasn't been tested yet whether it truly fails the build. It seems to have the same problem though, of having a binding there but not in the Qt implementation.
    
    Contributes to issue CURA-8640.

--- projects/python_packages.cmake
+++ projects/python_packages.cmake
@@ -12,3 +12,16 @@ if(BUILD_OS_WINDOWS)
         WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib/site-packages/cx_Freeze
     )
 endif()
+if(BUILD_OS_OSX)
+    # These are plug-ins in Qt where PyQt6-Qt6 does provide the bindings, but doesn't provide the binaries they need.
+    # Normally this isn't a problem, but on MacOS the packaging checks these dependencies and then fails to package them.
+    # Excluding them from the packager makes no difference. Deleting the files outright does.
+    add_custom_command(
+        TARGET PythonPackagesGeneral POST_BUILD
+        COMMAND rm -r "${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/PyQt6/Qt6/qml/QtQml/XmlListModel"
+        COMMAND rm -r "${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/PyQt6/Qt6/qml/QtQuick/Scene2D"
+        COMMAND rm -r "${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/PyQt6/Qt6/qml/QtQuick/Scene3D"
+        COMMAND rm -r "${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/PyQt6/Qt6/qml/QtQuick/LocalStorage"
+        COMMAND rm -r "${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/PyQt6/Qt6/qml/QtQuick/NativeStyle"
+    )
+endif()
