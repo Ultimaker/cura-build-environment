8ebde81ed75312ea3bc5ddd8c0f65018e4bd6c59
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-17 15:52:31 +0100

    Cleaned up Linux docker
    Made sure there are less scattered steps process
    
    Contribute to CURA-8640

--- docker/linux/entrypoint.sh
+++ docker/linux/entrypoint.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-#
-# The entrypoint script for the cura-build-environment CentOS docker image.
-#
-
-set -ex
-
-if [[ -z "${CURA_BUILD_ENV_PATH}" ]]; then
-    echo "CURA_BUILD_ENV_PATH is not defined. Could not find where cura-build-environment is installed."
+if [[ -z "${BUILD_ENV_PATH}" ]]; then
+    echo "BUILD_ENV_PATH is not defined. Could not find where cura-build-environment is installed."
     exit 1
 fi
-
-# Sets up the environment variables.
-export PATH="${CURA_BUILD_ENV_PATH}/bin:/usr/local/bin:${PATH}"
-export PKG_CONFIG_PATH="${CURA_BUILD_ENV_PATH}/lib/pkgconfig:${PKG_CONFIG_PATH}"
-export LD_LIBRARY_PATH="${CURA_BUILD_ENV_PATH}/lib:${LD_LIBRARY_PATH}"
 
 # Execute command
 exec "$@"
