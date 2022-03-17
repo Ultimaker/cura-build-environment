204410bbc384e60331d86bb32a9ed2d7dcb39a72
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-17 14:28:55 +0100

    Cleaned up pynest2d
    Contribute to CURA-8640

--- projects/pynest2d.cmake
+++ projects/pynest2d.cmake
@@ -1,31 +1,23 @@
-#Copyright (c) 2020 Ultimaker B.V.
+#Copyright (c) 2022 Ultimaker B.V.
 #cura-build-environment is released under the terms of the AGPLv3 or higher.
 
-set(pylib_cmake_command ${CMAKE_COMMAND})
+GetFromEnvironmentOrCache(
+        NAME
+            PYNEST2D_BRANCH_OR_TAG
+        DEFAULT
+            master
+        DESCRIPTION
+            "The name of the tag or branch to build for pynest2d")
 
 set(extra_cmake_args "")
 if(BUILD_OS_WINDOWS)
     set(extra_cmake_args -DCMAKE_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/libs -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX})
-else()
-  if(BUILD_OS_OSX)
-    if(CMAKE_OSX_DEPLOYMENT_TARGET)
-        list(APPEND extra_cmake_args
-            -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
-        )
-    endif()
-    if(CMAKE_OSX_SYSROOT)
-        list(APPEND extra_cmake_args
-            -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
-        )
-    endif()
-  endif()
 endif()
 
 ExternalProject_Add(pynest2d
     GIT_REPOSITORY https://github.com/Ultimaker/pynest2d.git
-    GIT_TAG origin/${CURA_PYNEST2D_BRANCH_OR_TAG}
+    GIT_TAG origin/${PYNEST2D_BRANCH_OR_TAG}
     GIT_SHALLOW 1
-    CMAKE_COMMAND ${pylib_cmake_command}
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
                -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
