7069a914afeb4a39e7ca085a46488987d0af073d
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-17 14:27:14 +0100

    Cleaned up Savitar
    Contribute to CURA-8640

--- projects/savitar.cmake
+++ projects/savitar.cmake
@@ -1,4 +1,13 @@
-set(savitar_cmake_command ${CMAKE_COMMAND})
+#Copyright (c) 2022 Ultimaker B.V.
+#cura-build-environment is released under the terms of the AGPLv3 or higher.
+
+GetFromEnvironmentOrCache(
+        NAME
+            SAVITAR_BRANCH_OR_TAG
+        DEFAULT
+            master
+        DESCRIPTION
+            "The name of the tag or branch to build for Savitar")
 
 set(extra_cmake_args "")
 if(BUILD_OS_WINDOWS)
@@ -6,26 +15,12 @@ if(BUILD_OS_WINDOWS)
       -DCMAKE_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/libs
 	  -DMSVC_STATIC_RUNTIME=OFF
     )
-else()
-  if(BUILD_OS_OSX)
-    if(CMAKE_OSX_DEPLOYMENT_TARGET)
-        list(APPEND extra_cmake_args
-            -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
-        )
-    endif()
-    if(CMAKE_OSX_SYSROOT)
-        list(APPEND extra_cmake_args
-            -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
-        )
-    endif()
-  endif()
 endif()
 
 ExternalProject_Add(Savitar
     GIT_REPOSITORY https://github.com/ultimaker/libSavitar.git
-    GIT_TAG origin/${CURA_SAVITAR_BRANCH_OR_TAG}
+    GIT_TAG origin/${SAVITAR_BRANCH_OR_TAG}
     GIT_SHALLOW 1
-    CMAKE_COMMAND ${savitar_cmake_command}
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
                -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
