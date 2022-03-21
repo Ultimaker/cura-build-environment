ab4e531ad54a6b3034459e7d519db0922897a759
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-21 13:51:18 +0100

    Set BUILD_ENV directories
    Contribute to CURA-8640

--- docker/linux/entrypoint.sh
+++ docker/linux/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [[ -z "${BUILD_ENV_PATH}" ]]; then
-    echo "BUILD_ENV_PATH is not defined. Could not find where cura-build-environment is installed."
-    exit 1
-fi
 
 # Execute command
 exec "$@"
