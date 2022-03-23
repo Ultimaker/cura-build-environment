333456c068944ff6b3e7afb2f02a243f0f488371
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-23 16:25:52 +0100

    Drop support for CTM files
    Open CTM has been used 5 times on million of slices
    the past year. Since OpenCTM seems to be abounded
    since 2012 we have decided to drop support. There
    are plenty of conversion tools for this file type
    available.
    
    Contributes to CURA-8640

--- projects/python_packages.cmake
+++ projects/python_packages.cmake
@@ -1,7 +1,7 @@
 add_custom_target(PythonPackagesGeneral ALL
     COMMAND ${Python3_EXECUTABLE} -m pip install --require-hashes -r  ${CMAKE_SOURCE_DIR}/projects/requirements.txt
     COMMENT "Install Python packages"
-    DEPENDS Python OpenCTM
+    DEPENDS Python
 )
 
 if(BUILD_OS_WINDOWS)
