dcf18277ca774a82d609750edba371bab8b2cd9e
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-23 16:05:15 +0100

    use autogen before the configure step
    Contributes to CURA-8640

--- projects/xz.cmake
+++ projects/xz.cmake
@@ -2,7 +2,7 @@ if(BUILD_OS_OSX)
     ExternalProject_Add(xz
         GIT_REPOSITORY https://git.tukaani.org/xz.git
         GIT_TAG v5.2.4
-        CONFIGURE_COMMAND ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX} --with-sysroot=${CMAKE_OSX_SYSROOT}
+        CONFIGURE_COMMAND ./autogen.sh && ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX} --with-sysroot=${CMAKE_OSX_SYSROOT}
         BUILD_COMMAND make -j ${N_PROC}
         INSTALL_COMMAND make install
         BUILD_IN_SOURCE 1
@@ -11,7 +11,7 @@ elseif(BUILD_OS_LINUX)
     ExternalProject_Add(xz
         GIT_REPOSITORY https://git.tukaani.org/xz.git
         GIT_TAG v5.2.4
-        CONFIGURE_COMMAND ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX}
+        CONFIGURE_COMMAND ./autogen.sh && ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX}
         BUILD_COMMAND make -j ${N_PROC}
         INSTALL_COMMAND make install
         BUILD_IN_SOURCE 1
