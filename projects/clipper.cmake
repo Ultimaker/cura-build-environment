c421315205c7a0d442fe73b4cb1fda2473a13ee2
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-23 16:23:15 +0100

    Apply patch to polyclipping runtime lib
    Contributes to CURA-8640

--- projects/clipper.cmake
+++ projects/clipper.cmake
@@ -1,14 +1,12 @@
-#Copyright (c) 2020 Ultimaker B.V.
+#Copyright (c) 2022 Ultimaker B.V.
 #cura-build-environment is released under the terms of the AGPLv3 or higher.
 
-#Clipper (dependency of libnest2d).
-
-#CMake 3.6 (our minimum requirement) doesn't support SOURCE_SUBDIR yet. Instead we copy the source from the cpp subfolder to the main folder.
 if(NOT TARGET Clipper)
     ExternalProject_Add(Clipper
         GIT_REPOSITORY https://github.com/skyrpex/clipper
         GIT_TAG 6.4.2
-        PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/cpp" "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper" && ${CMAKE_COMMAND} -E copy "${CMAKE_SOURCE_DIR}/projects/clipper_cmakelists_patch.txt" "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/CMakeLists.txt" && ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/clipper.hpp" "${CMAKE_INSTALL_PREFIX}/include/"
+        PATCH_COMMAND git apply ${CMAKE_SOURCE_DIR}/projects/0002-install-location-runtime-libs.patch
+        SOURCE_SUBDIR cpp
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                    -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
