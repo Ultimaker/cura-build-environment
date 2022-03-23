68a3f077593d9efd84704bc6a98f4ec1c88e9c95
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-23 15:49:10 +0100

    Use git as source
    Sourceforge is an unreliable download source.
    often failing to download and disputable
    practices
    
    https://www.howtogeek.com/218764/warning-don%E2%80%99t-download-software-from-sourceforge-if-you-can-help-it/
    
    Contribute to CURA-8640

--- projects/clipper.cmake
+++ projects/clipper.cmake
@@ -6,8 +6,8 @@
 #CMake 3.6 (our minimum requirement) doesn't support SOURCE_SUBDIR yet. Instead we copy the source from the cpp subfolder to the main folder.
 if(NOT TARGET Clipper)
     ExternalProject_Add(Clipper
-        URL https://sourceforge.net/projects/polyclipping/files/clipper_ver6.4.2.zip
-        URL_HASH SHA256=a14320d82194807c4480ce59c98aa71cd4175a5156645c4e2b3edd330b930627
+        GIT_REPOSITORY https://github.com/skyrpex/clipper
+        GIT_TAG 6.4.2
         PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/cpp" "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper" && ${CMAKE_COMMAND} -E copy "${CMAKE_SOURCE_DIR}/projects/clipper_cmakelists_patch.txt" "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/CMakeLists.txt" && ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_BINARY_DIR}/Clipper-prefix/src/Clipper/clipper.hpp" "${CMAKE_INSTALL_PREFIX}/include/"
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                    -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
--- projects/xz.cmake
+++ projects/xz.cmake
@@ -1,7 +1,7 @@
 if(BUILD_OS_OSX)
     ExternalProject_Add(xz
-        URL https://sourceforge.net/projects/lzmautils/files/xz-5.2.4.tar.gz
-        URL_HASH SHA256=b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145
+        GIT_REPOSITORY https://git.tukaani.org/xz.git
+        GIT_TAG v5.2.4
         CONFIGURE_COMMAND ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX} --with-sysroot=${CMAKE_OSX_SYSROOT}
         BUILD_COMMAND make -j ${N_PROC}
         INSTALL_COMMAND make install
@@ -9,8 +9,8 @@ if(BUILD_OS_OSX)
     )
 elseif(BUILD_OS_LINUX)
     ExternalProject_Add(xz
-        URL https://sourceforge.net/projects/lzmautils/files/xz-5.2.4.tar.gz
-        URL_HASH SHA256=b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145
+        GIT_REPOSITORY https://git.tukaani.org/xz.git
+        GIT_TAG v5.2.4
         CONFIGURE_COMMAND ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX}
         BUILD_COMMAND make -j ${N_PROC}
         INSTALL_COMMAND make install
