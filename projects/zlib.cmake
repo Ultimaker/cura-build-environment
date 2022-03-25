4065958933368da41e957b390c13bf810c269177
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-25 13:01:10 +0100

    Use CMAKE defined flags and compiler for zlib
    Contributes to CURA-8640

--- projects/zlib.cmake
+++ projects/zlib.cmake
@@ -2,7 +2,7 @@ if(BUILD_OS_OSX OR BUILD_OS_LINUX)
     ExternalProject_Add(zlib
         URL https://www.zlib.net/zlib-1.2.11.tar.gz
         URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
-        CONFIGURE_COMMAND ./configure --64 --prefix=${CMAKE_INSTALL_PREFIX}
+        CONFIGURE_COMMAND CXX_FLAGS="${CMAKE_CXX_FLAGS}"  CC="${CMAKE_C_COMPILER}"  CXX="${CMAKE_CXX_COMPILER}" ./configure --64 --prefix=${CMAKE_INSTALL_PREFIX}
         BUILD_COMMAND make -j ${N_PROC}
         INSTALL_COMMAND make install
         BUILD_IN_SOURCE 1
