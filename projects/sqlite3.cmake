1be4466548d3cae209334624f4dcfe7551f06b93
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-25 16:04:03 +0100

    Configure sqlite with compiler and flag
    Contributes to CURA-8640

--- projects/sqlite3.cmake
+++ projects/sqlite3.cmake
@@ -1,8 +1,23 @@
 if(BUILD_OS_OSX)
+    GetFromEnvironmentOrCache(
+            NAME
+                CMAKE_CXX_COMPILER
+            DEFAULT
+                clang++
+            DESCRIPTION
+                "Specify the CXX compiler to use")
+    GetFromEnvironmentOrCache(
+            NAME
+                CMAKE_C_COMPILER
+            DEFAULT
+                clang
+            DESCRIPTION
+                "Specify the C compiler to use")
+
     ExternalProject_Add(sqlite3
         URL https://sqlite.org/2018/sqlite-autoconf-3260000.tar.gz
         URL_HASH SHA256=5daa6a3fb7d1e8c767cd59c4ded8da6e4b00c61d3b466d0685e35c4dd6d7bf5d
-        CONFIGURE_COMMAND ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX} --with-sysroot=${CMAKE_OSX_SYSROOT}
+        CONFIGURE_COMMAND CXXFLAGS="-stdlib=libc++" CCX=${CMAKE_CXX_COMPILER} CC=${CMAKE_C_COMPILER} ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=${CMAKE_INSTALL_PREFIX} --with-sysroot=${CMAKE_OSX_SYSROOT}
         BUILD_COMMAND make -j ${N_PROC}
         INSTALL_COMMAND make install
         BUILD_IN_SOURCE 1
