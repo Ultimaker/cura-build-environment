f4e8bfece362d41c2b70a7a5d5965d8be6b5af44
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-28 10:26:48 +0200

    Use github as source for zlib
    Contributes to CURA-8640

--- projects/zlib.cmake
+++ projects/zlib.cmake
@@ -1,12 +1,12 @@
 if(BUILD_OS_OSX OR BUILD_OS_LINUX)
     ExternalProject_Add(zlib
-        URL https://www.zlib.net/zlib-1.2.11.tar.gz
-        URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
+        GIT_REPOSITORY https://github.com/madler/zlib.git
+        GIT_TAG v1.2.11
+        GIT_SHALLOW 1
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                    -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_CXX_STANDARD=17
                    -DAMD64=ON
-        BUILD_IN_SOURCE 1
     )
 endif()
