9416bd80319793d452f30f58ef1f6b8774f4fc97
Ghostkeeper <rubend@tutanota.com>
2022-04-01 16:38:19 +0200

    Fix Arcus and Pynest2D building environment
    These two needed to get the Python parameters to build properly.
    I also pass this to Savitar but that one needs some more work at the moment.
    
    Contributes to issue CURA-8640.

--- projects/arcus.cmake
+++ projects/arcus.cmake
@@ -2,22 +2,22 @@
 # cura-build-environment is released under the terms of the AGPLv3 or higher.
 
 GetFromEnvironmentOrCache(
-		NAME
-			ARCUS_BRANCH_OR_TAG
-		DEFAULT
-			master
-		DESCRIPTION
-			"The name of the tag (e.q.: v4.13.0), branch (e.q.: origin/CURA-8640) or commit hash (e.q.: 961dabf) for Arcus")
+        NAME
+            ARCUS_BRANCH_OR_TAG
+        DEFAULT
+            master
+        DESCRIPTION
+            "The name of the tag (e.q.: v4.13.0), branch (e.q.: origin/CURA-8640) or commit hash (e.q.: 961dabf) for Arcus")
 
 ExternalProject_Add(Arcus
-		GIT_REPOSITORY https://github.com/ultimaker/libArcus.git
-		GIT_TAG ${ARCUS_BRANCH_OR_TAG}
-		CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-				   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-				   -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
-				   -DSIP_BUILD_EXECUTABLE=${CMAKE_INSTALL_PREFIX}/bin/sip-build
-				   -DPYTHONPATH=${PYTHONPATH}
-				   -DPython_ROOT=${Python_ROOT}
-				   -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-		DEPENDS Python)
+        GIT_REPOSITORY https://github.com/Ultimaker/libArcus.git
+        GIT_TAG ${ARCUS_BRANCH_OR_TAG}
+        CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+                   -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
+                   -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+                   -DSIP_BUILD_EXECUTABLE=${CMAKE_INSTALL_PREFIX}/bin/sip-build
+                   -DPYTHONPATH=${PYTHONPATH}
+                   -DPython_SITEARCH=${Python_SITEARCH}
+        DEPENDS Python)
 add_dependencies(projects Arcus)
--- projects/savitar.cmake
+++ projects/savitar.cmake
@@ -10,14 +10,14 @@ GetFromEnvironmentOrCache(
             "The name of the tag (e.q.: v4.13.0), branch (e.q.: origin/CURA-8640) or commit hash (e.q.: 961dabf) for Savitar")
 
 ExternalProject_Add(Savitar
-		GIT_REPOSITORY https://github.com/ultimaker/libSavitar.git
-		GIT_TAG ${SAVITAR_BRANCH_OR_TAG}
-		CMAKE_COMMAND ${CMAKE_COMMAND} -E env "PYTHONPATH=${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages" ${CMAKE_COMMAND}
-		CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-				   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-				   -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
-				   -DSIP_BUILD_EXECUTABLE=${CMAKE_INSTALL_PREFIX}/bin/sip-build
-				   -DPython_ROOT=${Python_ROOT}
-		           -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-		DEPENDS Python)
+        GIT_REPOSITORY https://github.com/ultimaker/libSavitar.git
+        GIT_TAG ${SAVITAR_BRANCH_OR_TAG}
+        CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+                   -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
+                   -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+                   -DSIP_BUILD_EXECUTABLE=${CMAKE_INSTALL_PREFIX}/bin/sip-build
+                   -DPYTHONPATH=${PYTHONPATH}
+                   -DPython_SITEARCH=${Python_SITEARCH}
+        DEPENDS Python)
 add_dependencies(projects Savitar)
