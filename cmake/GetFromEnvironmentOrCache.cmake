d4b6f1aca05f35a8d08653a80ba35bfac024da8e
j.spijker@ultimaker.com <jelle spijker>
2022-04-06 07:17:47 +0200

    Fixed version defaults
    somehow a difference between hoe it's handled on Mac and Linux
    
    Contributes to CURA-8640

--- cmake/GetFromEnvironmentOrCache.cmake
+++ cmake/GetFromEnvironmentOrCache.cmake
@@ -1,6 +1,6 @@
 # Copyright 2022 Ultimaker
 
-function(GetFromEnvironmentOrCache)
+macro(GetFromEnvironmentOrCache)
     set(options BOOL FILEPATH PATH STRING INTERNAL REQUIRED)
     set(oneValueArgs NAME DEFAULT DESCRIPTION)
     set(multiValueArgs )
@@ -31,11 +31,10 @@ function(GetFromEnvironmentOrCache)
         elseif(DEFINED VAR_DEFAULT)
             set(VAR_VALUE ${VAR_DEFAULT})
             message(STATUS "Using default value for ${VAR_NAME}: ${VAR_VALUE}")
-        endif()
-        if(VAR_REQUIRED AND NOT VAR_VALUE)
+        elseif(VAR_REQUIRED)
             message(SEND_ERROR "${VAR_NAME} is a required variable, either provide a commandline arg, environment variable or default value")
         else()
-            set(VAR_VALUE "")
+            set(VAR_VALUE )
             message(STATUS "Using empty default value for ${VAR_NAME}")
         endif()
 
@@ -43,15 +42,13 @@ function(GetFromEnvironmentOrCache)
             set(VAR_DESCRIPTION "")
         endif()
 
-        if(DEFINED VAR_VALUE)
-            set(${VAR_NAME}
-                    ${VAR_VALUE}
-                    CACHE
-                    ${VAR_CACHE}
-                    ${VAR_DESCRIPTION}
-                    FORCE)
-        endif()
+        set(${VAR_NAME}
+                "${VAR_VALUE}"
+                CACHE
+                ${VAR_CACHE}
+                ${VAR_DESCRIPTION}
+                FORCE)
     else()
         message(STATUS "Using CMake provided variable ${VAR_NAME}: ${${VAR_NAME}}")
     endif()
-endfunction()
+endmacro()
