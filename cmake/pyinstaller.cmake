7fa8a7b017b9b4d101833b7fdefab797e7b3a949
j.spijker@ultimaker.com <jelle spijker>
2022-04-06 09:39:21 +0200

    Fixed paths
    Contributes to CURA-8640

--- cmake/pyinstaller.cmake
+++ cmake/pyinstaller.cmake
@@ -7,11 +7,11 @@
 set(pyinstaller_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/pyinstaller)
 set(cura_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/cura_app.py)
 set(curaengine_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/CuraEngine)
-set(installer_DIR ${CMAKE_INSTALL_PREFIX}/installer)
-set(ULTIMAKER_CURA_PATH ${installer_DIR}/dist/Ultimaker-Cura)
+set(installer_DIR "${CMAKE_INSTALL_PREFIX}/installer")
+set(ULTIMAKER_CURA_PATH "${installer_DIR}/dist/Ultimaker-Cura")
 
 if (APPLE)
-    set(ULTIMAKER_CURA_APP_PATH ${ULTIMAKER_CURA_PATH}.app)
+    set(ULTIMAKER_CURA_APP_PATH "${ULTIMAKER_CURA_PATH}.app")
     GetFromEnvironmentOrCache(
             NAME
                 SIGN_DMG
@@ -20,7 +20,6 @@ if (APPLE)
             BOOL
             DESCRIPTION
                 "Sign the dmg")
-
     if (SIGN_DMG)
         GetFromEnvironmentOrCache(
                 NAME
@@ -35,7 +34,7 @@ if (APPLE)
                 nl.ultimaker.cura.dmg
             DESCRIPTION
                 "The Ultimaker Cura domain to be used (usually reversed)")
-        set(extra_pyinstaller_args "--codesign-identity \"${CODESIGN_IDENTITY}\" --osx-entitlements-file \"${CMAKE_SOURCE_DIR}\\signing\\cura.entitlements\" --osx-bundle-identifier \"${ULTIMAKER_CURA_DOMAIN}\" ")
+        set(extra_pyinstaller_args --codesign-identity "${CODESIGN_IDENTITY}" --osx-entitlements-file "${CMAKE_SOURCE_DIR}/signing/cura.entitlements" --osx-bundle-identifier "${ULTIMAKER_CURA_DOMAIN}" )
     endif ()
 else ()
     set(extra_pyinstaller_args)
@@ -46,27 +45,27 @@ add_custom_target(pyinstaller ALL COMMENT "Collect the build artifacts in a sing
 
 add_custom_command(
         TARGET
-        pyinstaller
+            pyinstaller
         WORKING_DIRECTORY
-        ${installer_DIR}
+            ${installer_DIR}
         COMMAND
-        ${CMAKE_COMMAND} -E env "PYTHONPATH=${PYTHONPATH}" ${pyinstaller_EXECUTABLE} ${cura_EXECUTABLE}
-        --collect-all cura
-        --collect-all UM
-        --collect-all serial #Used only in plug-ins.
-        --collect-all Charon
-        --hidden-import pySavitar
-        --hidden-import pyArcus
-        --hidden-import pynest2d
-        --hidden-import PyQt6.QtNetwork
-        --hidden-import zeroconf
-        --add-binary "${curaengine_EXECUTABLE}:."
-        --add-data "${CMAKE_INSTALL_PREFIX}/lib/cura/plugins:plugins"
-        --add-data "${CMAKE_INSTALL_PREFIX}/lib/uranium/plugins:plugins"
-        --add-data "${CMAKE_INSTALL_PREFIX}/share/cura/resources:resources"
-        --add-data "${CMAKE_INSTALL_PREFIX}/share/uranium/resources:resources"
-        --add-data "${Python_SITELIB_LOCAL}/UM/Qt/qml/UM/:resources/qml/UM/"
-        --windowed --clean --noconfirm --log-level INFO ${extra_pyinstaller_args}
-        --name "Ultimaker-Cura"
+            ${CMAKE_COMMAND} -E env "PYTHONPATH=${PYTHONPATH}" ${pyinstaller_EXECUTABLE} ${cura_EXECUTABLE}
+                --collect-all cura
+                --collect-all UM
+                --collect-all serial #Used only in plug-ins.
+                --collect-all Charon
+                --hidden-import pySavitar
+                --hidden-import pyArcus
+                --hidden-import pynest2d
+                --hidden-import PyQt6.QtNetwork
+                --hidden-import zeroconf
+                --add-binary "${curaengine_EXECUTABLE}:."
+                --add-data "${CMAKE_INSTALL_PREFIX}/lib/cura/plugins:plugins"
+                --add-data "${CMAKE_INSTALL_PREFIX}/lib/uranium/plugins:plugins"
+                --add-data "${CMAKE_INSTALL_PREFIX}/share/cura/resources:resources"
+                --add-data "${CMAKE_INSTALL_PREFIX}/share/uranium/resources:resources"
+                --add-data "${Python_SITELIB_LOCAL}/UM/Qt/qml/UM/:resources/qml/UM/"
+                --windowed --clean --noconfirm --log-level INFO ${extra_pyinstaller_args}
+                --name "Ultimaker-Cura"
         DEPENDS install-python-requirements Cura create_installer_dir)
 add_dependencies(pyinstaller install-python-requirements Cura create_installer_dir)
