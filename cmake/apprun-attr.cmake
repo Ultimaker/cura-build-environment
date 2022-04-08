5c1eb165f19b499a26bffc9c4889b6d1c4a3ce2a
j.spijker@ultimaker.com <jelle spijker>
2022-04-08 16:23:47 +0200

    Fix the permission of the AppRun
    Contributes to CURA-8640

--- cmake/apprun-attr.cmake
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2022 Ultimaker B.V.
-# Cura's build system is released under the terms of the AGPLv3 or higher.
-
-file(COPY${CMAKE_SOURCE_DIR}/packaging/cura.sh
-     DESTINATION ${ULTIMAKER_CURA_PATH}
-     FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_WRITE GROUP_READ WORLD_READ OWNER_EXECUTE)
