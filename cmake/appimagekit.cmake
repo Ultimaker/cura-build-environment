d5dbe8145b37a77fa7664ee49bdf7055a8b0119e
j.spijker@ultimaker.com <jelle spijker>
2022-04-08 13:06:25 +0200

    Allow for the package appimage target to work in/out side of docker
    Setting the APPIMAGE_EXTRA_AND_RUN=1 as ENV
    allows reusing the target packaging on a non docker environment
    
    https://github.com/AppImage/AppImageKit/issues/912#issuecomment-528669441
    
    Contributes to CURA-8640

--- cmake/appimagekit.cmake
+++ cmake/appimagekit.cmake
@@ -13,8 +13,8 @@ add_custom_target(packaging ALL
     COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/cura.desktop ${ULTIMAKER_CURA_PATH}/cura.desktop
     COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/packaging/cura.sh ${ULTIMAKER_CURA_PATH}/AppRun
     COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/packaging/cura.appdata.xml ${ULTIMAKER_CURA_PATH}/cura.appdata.xml
-    COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_CURRENT_BINARY_DIR}/${INSTALLER_FILENAME}  #Remove old one if it existed.
-    COMMAND appimage --appimage-extract-and-run ${ULTIMAKER_CURA_PATH} ${CMAKE_CURRENT_BINARY_DIR}/${INSTALLER_FILENAME}
+    COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_CURRENT_BINARY_DIR}/${INSTALLER_FILENAME}
+    COMMAND appimage ${ULTIMAKER_CURA_PATH} ${CMAKE_CURRENT_BINARY_DIR}/${INSTALLER_FILENAME}
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
 add_dependencies(packaging pyinstaller)
