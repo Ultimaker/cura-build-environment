875adff48708462cf9e9365cedf48dbbef17b3c9
j.spijker@ultimaker.com <jelle spijker>
2022-04-09 10:33:22 +0200

    Allow installer filename postfix to be set via CMake
    Contributes to Cura-8640

--- cmake/installer-filename.cmake
+++ cmake/installer-filename.cmake
@@ -1,6 +1,28 @@
 # Copyright (c) 2022 Ultimaker B.V.
 # Cura's build system is released under the terms of the AGPLv3 or higher.
 
+GetFromEnvironmentOrCache(
+        NAME
+            POSTFIX_OS_INSTALLER_FILENAME_WINDOWS
+        DEFAULT
+            win64
+        DESCRIPTION
+            "postfix for the installer file name when a Windows build is made (default win64): Ultimaker-Cura-5.0.0-b+2-win64.exe")
+GetFromEnvironmentOrCache(
+        NAME
+            POSTFIX_OS_INSTALLER_FILENAME_LINUX
+        DEFAULT
+            linux
+        DESCRIPTION
+            "postfix for the installer file name when a Windows build is made (default linux): Ultimaker-Cura-5.0.0-b+2-linux.AppImage")
+GetFromEnvironmentOrCache(
+        NAME
+            POSTFIX_OS_INSTALLER_FILENAME_MAC
+        DEFAULT
+            mac
+        DESCRIPTION
+            "postfix for the installer file name when a Windows build is made (default mac): Ultimaker-Cura-5.0.0-b+2-mac.dmg")
+
 set(INSTALLER_BASE_FILENAME "Ultimaker-Cura")
 if(NOT CURA_BUILDTYPE STREQUAL "")
     string(REPLACE " " "_" CURA_BUILDTYPE_NO_SPACES "${CURA_BUILDTYPE}")
@@ -9,15 +31,15 @@ endif()
 set(INSTALLER_BASE_FILENAME ${INSTALLER_BASE_FILENAME}-${CURA_VERSION})
 
 if(WIN32)
-    set(INSTALLER_BASE_FILENAME ${INSTALLER_BASE_FILENAME}-windows)
+    set(INSTALLER_BASE_FILENAME ${INSTALLER_BASE_FILENAME}-${POSTFIX_OS_INSTALLER_FILENAME_WINDOWS})
 endif()
 
 if(LINUX)
-    set(INSTALLER_BASE_FILENAME ${INSTALLER_BASE_FILENAME}-linux)
+    set(INSTALLER_BASE_FILENAME ${INSTALLER_BASE_FILENAME}-${POSTFIX_OS_INSTALLER_FILENAME_LINUX})
 endif()
 
 if(APPLE)
-    set(INSTALLER_BASE_FILENAME ${INSTALLER_BASE_FILENAME}-mac)
+    set(INSTALLER_BASE_FILENAME ${INSTALLER_BASE_FILENAME}-${POSTFIX_OS_INSTALLER_FILENAME_MAC})
 endif()
 
 if(INSTALLER_EXT)
