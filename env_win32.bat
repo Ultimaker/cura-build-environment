20492a61d55d3a193ff32ab3834f80ad6f1530bb
Jelle Spijker <spijker.jelle@gmail.com>
2022-04-12 16:28:09 +0200

    Remove olf environements scritps
    Contributes to CURA-8640

--- env_win32.bat
+++
@@ -1,5 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-
-REM cryptography by default links to OpenSSL 1.1.0 which has different library
-REM file names, so we need this flag to be able to link to OpenSSL 1.0.2
-set CRYPTOGRAPHY_WINDOWS_LINK_LEGACY_OPENSSL=1
--- env_win64.bat
+++
@@ -1,5 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-set CFLAGS=-m64
-set CXXFLAGS=-m64
-set LDFLAGS=-m64
-
