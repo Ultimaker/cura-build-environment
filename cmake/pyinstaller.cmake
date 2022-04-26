7dff44636bfb5f1928c8873abdf48b1db23bb9d2
j.delarago <joeydelarago@gmail.com>
2022-04-26 09:19:29 +0200

    remove evironment variable

--- cmake/pyinstaller.cmake
+++ cmake/pyinstaller.cmake
@@ -42,7 +42,6 @@ if(LINUX)
 endif()
 
 if (WIN32)
-    set(PYINSTALLER_COMPILE_BOOTLOADER=1)
     list(APPEND extra_pyinstaller_args --hidden-import fcntl --collect-all win32ctypes --icon ${CMAKE_SOURCE_DIR}/packaging/Cura.ico)
 endif ()
 
