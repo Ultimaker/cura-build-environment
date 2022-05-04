75d9b6d1019237290f6c7ed800ae3bad83fcf976
Evangelos Trantos <evtrados@gmail.com>
2022-05-04 16:10:33 +0200

    Delete sign-appimage.cmake
    This file is not needed anymore, since we create the .asc file using the rundeck scripts.

--- cmake/sign-appimage.cmake
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2022 Ultimaker B.V.
-# Cura's build system is released under the terms of the AGPLv3 or higher.
-
-add_custom_target(signing ALL COMMENT "Create the asc file for the AppImage file.")
-#add_custom_command(
-#        TARGET
-#            signing
-#        COMMAND sha256sum ${CMAKE_CURRENT_BINARY_DIR}/${INSTALLER_FILENAME} >> ${CMAKE_CURRENT_BINARY_DIR}/${INSTALLER_BASE_FILENAME}.asc)
-add_dependencies(signing packaging)
