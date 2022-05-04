d9d0ae7832379831b03b51e62d933ed033d6a96b
Evangelos Trantos <evtrados@gmail.com>
2022-05-04 08:33:09 +0200

    Remove pyinstaller.py first before installing
    It seems that there is an issue when installing pyinstaller from source using the setup.py script, which is caused by quote "the latest setuptools in combination with sdist containing both pyinstaller.py script and PyInstaller package (which end up shadowing each other on case-insensitive OSes, i.e., Windows in macOS)".
    The solution to that is to remove the pyinstaller.py scripts since it's not being used anyway.

--- projects/pyinstaller_win.cmake
+++ projects/pyinstaller_win.cmake
@@ -6,7 +6,10 @@ if(WIN32)
         URL https://github.com/pyinstaller/pyinstaller/archive/v4.10.tar.gz
         URL_HASH SHA256=bd388f25dbe1f13274240319638c1145935476701c453b89589b59cf3cb47895
         CONFIGURE_COMMAND ""
-        BUILD_COMMAND ${CMAKE_COMMAND} -E chdir bootloader ${Python_VENV_EXECUTABLE} ./waf all
+        BUILD_COMMAND ${CMAKE_COMMAND} -E chdir bootloader ${Python_VENV_EXECUTABLE} ./waf all # This builds the bootloader
+        # Remove the pyinstaller.py script so that we can then successfully install the package 
+        # (see https://github.com/pyinstaller/pyinstaller/issues/6704#issuecomment-1079916268)
+        COMMAND powershell -Command Remove-Item -Force pyinstaller.py
         INSTALL_COMMAND ${Python_VENV_EXECUTABLE} setup.py install
         BUILD_IN_SOURCE 1
     )
